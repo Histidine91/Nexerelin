060152974cbe7d9be8749cbbb24c5b6342244124
Kirk Wilson <kirkzw@gmail.com>
2013-06-30 20:35:25 +1000

    - Added starting option to start with nine other factions  - Added starting option to respawn factions only up to the starting number  - Added starting option to have no asteroid belts (apart from one around the star)  - Added asteroid mining fleets for stations    - Mining fleets go to the nearest asteroid and slowly mine to create supplies  - Added gas mining fleets for stations    - Mining fleets go to the nearest gas giant and slowly mine to create fuel
     - Changed faction alliance mechanic
       - If two factions are allied, they must now also each be at war with each others enemies
       - As wars/alliances start/end, factions will decide whether to join their ally or cancel the alliance
     - Changed each system to always spawn with an asteroid belt around the star
     - Changed each system to spawn with at least one gas giant
     - Changed Patrol Fleets to take on extra assignments when at war
       - Patrol Fleet can now help Attack Fleets
       - Patrol Fleets can now raid system searching for enemy fleets
    
     - Fixed null reference exception with in system supply convoys
     - Fixed issue with catastrophic station accidents not occurring
     - Fixed (another) issue with ships/weapons not clearing properly on station takeover
     - Fixed issue where factions could respawn even though respawn was off
     - Fixed issue calculating last faction in certain circumstances
    
     - In System Supply Convoys (spawned form stations) now also deliver ships/weapons if delivering to player stations
       - Allied convoys will deliver allied faction ships/weapons
     - Rebels will now attack Trade Guild fleets

---
+++ data/scripts/world/exerelin/AsteroidMiningFleetSpawnPoint.java
@@ -0,0 +1,132 @@
+package data.scripts.world.exerelin;
+
+import com.fs.starfarer.api.Script;
+import com.fs.starfarer.api.campaign.*;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import data.scripts.world.BaseSpawnPoint;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+public class AsteroidMiningFleetSpawnPoint extends BaseSpawnPoint
+{
+	CampaignFleetAPI theFleet;
+	String owningFactionId;
+	SectorEntityToken targetAsteroid;
+	Boolean returningHome = false;
+	float fleetCargoCapacity = 0;
+
+	public AsteroidMiningFleetSpawnPoint(SectorAPI sector, LocationAPI location,
+									float daysInterval, int maxFleets, SectorEntityToken anchor)
+	{
+		super(sector, location, daysInterval, maxFleets, anchor);
+	}
+
+	public void setFaction(String factionId)
+	{
+		owningFactionId = factionId;
+		while(this.getFleets().size() > 0)
+			this.getFleets().remove(0);
+	}
+
+	public void setTargetAsteroid(SectorEntityToken asteroid)
+	{
+		//if(asteroid != null && targetAsteroid != null && targetAsteroid.getFullName().equalsIgnoreCase(asteroid.getFullName()))
+		//	return; // No change
+
+		targetAsteroid = asteroid;
+		for(int i = 0; i < this.getFleets().size();i++)
+			setFleetAssignments((CampaignFleetAPI)this.getFleets().get(i));
+	}
+
+	@Override
+	public CampaignFleetAPI spawnFleet() {
+		String type = "exerelinAsteroidMiningFleet";
+
+		if(this.getFleets().size() == this.getMaxFleets())
+			return null;
+
+		if(targetAsteroid == null)
+			return null;
+
+		// Create fleet
+		CampaignFleetAPI fleet = getSector().createFleet(owningFactionId, type);
+		fleet.setName("Asteroid Mining Fleet");
+		theFleet = fleet;
+		getLocation().spawnFleet(getAnchor(), 0, 0, fleet);
+
+		fleet.setPreferredResupplyLocation(getAnchor());
+
+		returningHome = false;
+		fleetCargoCapacity = getFleetCargoCapacity(fleet);
+		setFleetAssignments(fleet);
+
+		this.getFleets().add(fleet);
+		return fleet;
+	}
+
+	private void setFleetAssignments(CampaignFleetAPI fleet)
+	{
+		fleet.clearAssignments();
+		if(targetAsteroid != null)
+		{
+			if(!returningHome)
+				fleet.addAssignment(FleetAssignment.GO_TO_LOCATION, targetAsteroid, 1000, createTestTargetScript());
+			else
+				fleet.addAssignment(FleetAssignment.GO_TO_LOCATION, getAnchor(), 1000, createTestTargetScript());
+			fleet.addAssignment(FleetAssignment.GO_TO_LOCATION_AND_DESPAWN, getAnchor(), 1000);
+		}
+		else
+		{
+			fleet.addAssignment(FleetAssignment.GO_TO_LOCATION_AND_DESPAWN, getAnchor(), 1000);
+		}
+	}
+
+	private Script createTestTargetScript() {
+		return new Script() {
+			public void run() {
+				if(!returningHome && theFleet.getCargo().getSupplies() < fleetCargoCapacity)
+				{
+					// Mine more supplies
+					theFleet.getCargo().addSupplies(1);
+				}
+				else if(!returningHome)
+				{
+					// Head for home
+					returningHome = true;
+				}
+				else if (theFleet.getCargo().getSupplies() > 20)
+				{
+					// Reached home so unload
+					theFleet.getCargo().removeSupplies(1);
+					getAnchor().getCargo().addSupplies(1);
+				}
+				else
+				{
+					// Head out to mine again
+					returningHome = false;
+					fleetCargoCapacity = getFleetCargoCapacity(theFleet);
+				}
+
+				setFleetAssignments(theFleet);
+			}
+		};
+	}
+
+	private float getFleetCargoCapacity(CampaignFleetAPI fleet)
+	{
+		float capacity = 0;
+		List members = fleet.getFleetData().getMembersListCopy();
+		for(int i = 0; i < members.size(); i++)
+		{
+			FleetMemberAPI fmAPI = (FleetMemberAPI)members.get(i);
+			capacity = capacity + fmAPI.getCargoCapacity();
+		}
+		return (int)(capacity*1.3);
+	}
+}
+
+
+
+
+
+
---
+++ data/scripts/world/exerelin/GasMiningFleetSpawnPoint.java
@@ -0,0 +1,135 @@
+package data.scripts.world.exerelin;
+
+import com.fs.starfarer.api.Script;
+import com.fs.starfarer.api.campaign.*;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import data.scripts.world.BaseSpawnPoint;
+
+import java.util.List;
+
+@SuppressWarnings("unchecked")
+public class GasMiningFleetSpawnPoint extends BaseSpawnPoint
+{
+	CampaignFleetAPI theFleet;
+	String owningFactionId;
+	SectorEntityToken targetPlanet;
+	Boolean returningHome = false;
+	float fleetFuelCapacity = 0;
+
+	public GasMiningFleetSpawnPoint(SectorAPI sector, LocationAPI location,
+									  float daysInterval, int maxFleets, SectorEntityToken anchor)
+	{
+		super(sector, location, daysInterval, maxFleets, anchor);
+	}
+
+	public void setFaction(String factionId)
+	{
+		owningFactionId = factionId;
+		while(this.getFleets().size() > 0)
+			this.getFleets().remove(0);
+	}
+
+	public void setTargetPlanet(SectorEntityToken planet)
+	{
+		if(planet != null && targetPlanet != null && targetPlanet.getFullName().equalsIgnoreCase(planet.getFullName()))
+			return; // No change
+
+		targetPlanet = planet;
+		for(int i = 0; i < this.getFleets().size();i++)
+			setFleetAssignments((CampaignFleetAPI)this.getFleets().get(i));
+	}
+
+	@Override
+	public CampaignFleetAPI spawnFleet() {
+		String type = "exerelinGasMiningFleet";
+
+		if(this.getFleets().size() == this.getMaxFleets())
+			return null;
+
+		if(targetPlanet == null)
+			return null;
+
+		// Create fleet
+		CampaignFleetAPI fleet = getSector().createFleet(owningFactionId, type);
+		fleet.setName("Gas Mining Fleet");
+		theFleet = fleet;
+		getLocation().spawnFleet(getAnchor(), 0,0, fleet);
+
+		fleet.setPreferredResupplyLocation(getAnchor());
+
+		returningHome = false;
+		fleetFuelCapacity = getFleetFuelCapacity(fleet);
+		setFleetAssignments(fleet);
+
+		this.getFleets().add(fleet);
+		return fleet;
+	}
+
+	private void setFleetAssignments(CampaignFleetAPI fleet)
+	{
+		fleet.clearAssignments();
+		if(targetPlanet != null)
+		{
+			if(!returningHome)
+				//fleet.addAssignment(FleetAssignment.GO_TO_LOCATION, getLocation().createToken(targetPlanet.getLocation().getX() + ExerelinUtils.getRandomInRange(-100,100), targetPlanet.getLocation().getY() + ExerelinUtils.getRandomInRange(-100,100)), 1000, createTestTargetScript());
+				fleet.addAssignment(FleetAssignment.GO_TO_LOCATION, targetPlanet, 1000, createTestTargetScript());
+			else
+				fleet.addAssignment(FleetAssignment.GO_TO_LOCATION, getAnchor(), 1000, createTestTargetScript());
+			fleet.addAssignment(FleetAssignment.GO_TO_LOCATION_AND_DESPAWN, getAnchor(), 1000);
+		}
+		else
+		{
+			fleet.addAssignment(FleetAssignment.GO_TO_LOCATION_AND_DESPAWN, getAnchor(), 1000);
+		}
+	}
+
+	private Script createTestTargetScript() {
+		return new Script() {
+			public void run() {
+				if(!returningHome && theFleet.getCargo().getFuel() < fleetFuelCapacity)
+				{
+					// Mine more gas
+					theFleet.getCargo().addFuel(3);
+				}
+				else if(!returningHome)
+				{
+					// Head for home
+					returningHome = true;
+				}
+				else if (theFleet.getCargo().getFuel() > 0)
+				{
+					// Reached home so unload
+					theFleet.getCargo().removeFuel(1);
+					getAnchor().getCargo().addFuel(1);
+				}
+				else
+				{
+					// Head out to mine again
+					returningHome = false;
+					fleetFuelCapacity = getFleetFuelCapacity(theFleet);
+				}
+
+
+				setFleetAssignments(theFleet);
+			}
+		};
+	}
+
+	private float getFleetFuelCapacity(CampaignFleetAPI fleet)
+	{
+		float capacity = 0;
+		List members = fleet.getFleetData().getMembersListCopy();
+		for(int i = 0; i < members.size(); i++)
+		{
+			FleetMemberAPI fmAPI = (FleetMemberAPI)members.get(i);
+			capacity = capacity + fmAPI.getFuelCapacity();
+		}
+		return (int)(capacity*1.3);
+	}
+}
+
+
+
+
+
+
