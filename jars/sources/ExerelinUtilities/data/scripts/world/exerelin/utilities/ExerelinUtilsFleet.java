0fab50df745f1ecf8fee76e4926253826b43a9b7
Kirk Wilson <kirkzw@gmail.com>
2013-10-15 23:08:48 +1100

    TECHNICAL  - Code cleanup

--- jars/sources/ExerelinUtilities/data/scripts/world/exerelin/utilities/ExerelinUtilsFleet.java
+++ jars/sources/ExerelinUtilities/data/scripts/world/exerelin/utilities/ExerelinUtilsFleet.java
@@ -5,7 +5,6 @@ import com.fs.starfarer.api.fleet.FleetMemberAPI;
 
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExerelinUtilsFleet
