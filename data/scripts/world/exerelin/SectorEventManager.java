c4f6b1f29ba5ef4d8be083bfb065acbdb76fa8f2
Kirk Wilson <kirkzw@gmail.com>
2013-10-27 19:50:21 +1100

    - Fixed diplomacy item spawn chance

--- data/scripts/world/exerelin/SectorEventManager.java
+++ data/scripts/world/exerelin/SectorEventManager.java
@@ -23,7 +23,7 @@ public class SectorEventManager
 
         float extraChance = ExerelinUtilsPlayer.getPlayerDiplomacyObjectCreationBonus();
 
-        if(ExerelinUtils.getRandomInRange(0,(40*(int)(1.0f - extraChance))) == 0
+        if(ExerelinUtils.getRandomInRange(0,(int)(45*(1.0f - extraChance))) == 0
                 && !eventAddObjectToStorage.getType().equalsIgnoreCase(lastEventType))
         {
             eventAddObjectToStorage.addAgentToStorageFacility();
@@ -32,7 +32,7 @@ public class SectorEventManager
             lastEventType = "";
         }
 
-        if(ExerelinUtils.getRandomInRange(0,(40*(int)(1.0f - extraChance))) == 0
+        if(ExerelinUtils.getRandomInRange(0,(int)(45*(1.0f - extraChance))) == 0
                 && !eventAddObjectToStorage.getType().equalsIgnoreCase(lastEventType))
         {
             eventAddObjectToStorage.addPrisonerToStorageFacility();
@@ -41,7 +41,7 @@ public class SectorEventManager
             lastEventType = "";
         }
 
-        if(ExerelinUtils.getRandomInRange(0,(40*(int)(1.0f - extraChance))) == 0
+        if(ExerelinUtils.getRandomInRange(0,(int)(45*(1.0f - extraChance))) == 0
                 && !eventAddObjectToStorage.getType().equalsIgnoreCase(lastEventType)
                 && ExerelinUtilsPlayer.getPlayerSabateurAvailability())
         {
