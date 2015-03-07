b1e7b3161be9bdc63094d83ffa40ff3bbb9d6000
L.J. Lim <kingraptorowns@gmail.com>
2015-03-07 21:51:40 +0800

    Agents and saboteurs produced more slowly; decay faster

--- jars/sources/ExerelinCore/exerelin/campaign/econ/ExerelinMilitaryBase.java
+++ jars/sources/ExerelinCore/exerelin/campaign/econ/ExerelinMilitaryBase.java
@@ -6,8 +6,8 @@ public class ExerelinMilitaryBase extends MilitaryBase {
 
 	public void apply(String id) {
 		super.apply(id);
-		market.getCommodityData("agent").getSupply().modifyFlat(id, 4);
-		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 2);
+		market.getCommodityData("agent").getSupply().modifyFlat(id, 2);
+		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 1);
 	}
 
 	public void unapply(String id) {
