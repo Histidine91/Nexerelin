449dfcc1d3c140111f5c45f5865985372ad3ccee
L.J. Lim <kingraptorowns@gmail.com>
2015-03-27 21:42:49 +0800

    Don't crash if Exerelin star is renamed (Also add some new planet/star names because why not)

---
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/NGCSetStartingLocationExerelin.java
@@ -0,0 +1,37 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.rules.MemKeys;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.characters.CharacterCreationPlugin.CharacterCreationData;
+import com.fs.starfarer.api.util.Misc;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.apache.log4j.Level;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class NGCSetStartingLocationExerelin extends BaseCommandPlugin {
+    
+    @Override
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        String homestar = "Exerelin";
+        try {
+                JSONObject planetConfig = Global.getSettings().loadJSON("data/config/planetNames.json");
+                JSONArray systemNames = planetConfig.getJSONArray("stars");
+                homestar = systemNames.getString(0);
+
+        } catch (JSONException | IOException ex) {
+                Global.getLogger(NGCSetStartingLocationExerelin.class).log(Level.ERROR, ex);
+        }
+        CharacterCreationData data = (CharacterCreationData) memoryMap.get(MemKeys.LOCAL).get("$characterData");
+	data.setStartingLocationName(homestar);
+	data.getStartingCoordinates().set(300, 300);
+        
+        return true;
+    }
+}
