0af132362e0505d65fa8abaadda83cee408941cb
L.J. Lim <kingraptorowns@gmail.com>
2015-04-12 01:57:24 +0800

    ExerelinSetupData: indents

--- jars/sources/ExerelinCore/exerelin/campaign/ExerelinSetupData.java
+++ jars/sources/ExerelinCore/exerelin/campaign/ExerelinSetupData.java
@@ -17,61 +17,61 @@ import org.apache.log4j.Logger;
 @SuppressWarnings("unchecked")
 public final class ExerelinSetupData
 {
-    public static Logger log = Global.getLogger(ExerelinSetupData.class);
-    private static ExerelinSetupData instance = null;
+	public static Logger log = Global.getLogger(ExerelinSetupData.class);
+	private static ExerelinSetupData instance = null;
 
-    // Player setup defaults
-    private String[] availableFactions = null;
+	// Player setup defaults
+	private String[] availableFactions = null;
 
-    // Sector Generation Defaults
-    public int numSystems = 6;
-    public int maxMoonsPerPlanet = 2;
+	// Sector Generation Defaults
+	public int numSystems = 6;
+	public int maxMoonsPerPlanet = 2;
 
-    public int maxPlanets = 5;
-    public int maxStations = 3;
-    public int maxAsteroidBelts = 3;
-    public int maxSystemSize = 14000;
-    public int maxSectorSize = 12000;
+	public int maxPlanets = 5;
+	public int maxStations = 3;
+	public int maxAsteroidBelts = 3;
+	public int maxSystemSize = 14000;
+	public int maxSectorSize = 12000;
 
-    // Game defaults
-    public Boolean playerOwnedStationFreeTransfer = false;
-    public Boolean confirmedFreeTransfer = false;
-    public boolean respawnFactions = false;
-    public boolean onlyRespawnStartingFactions = false;
-    public int respawnDelay = 60;
-    public int numStartFactions = 3;
-    public int maxFactionsInExerelinAtOnce = 16;
-    public boolean randomStartRelationships = false;
-    public boolean omnifactoryPresent = false;
-    public boolean randomOmnifactoryLocation = false;
-    public boolean prismMarketPresent = false;
-    public boolean isSectorPopulated = false;
-    public boolean isSectorPartiallyPopulated = false;
-    public boolean freeStart = false;
+	// Game defaults
+	public Boolean playerOwnedStationFreeTransfer = false;
+	public Boolean confirmedFreeTransfer = false;
+	public boolean respawnFactions = false;
+	public boolean onlyRespawnStartingFactions = false;
+	public int respawnDelay = 60;
+	public int numStartFactions = 3;
+	public int maxFactionsInExerelinAtOnce = 16;
+	public boolean randomStartRelationships = false;
+	public boolean omnifactoryPresent = false;
+	public boolean randomOmnifactoryLocation = false;
+	public boolean prismMarketPresent = false;
+	public boolean isSectorPopulated = false;
+	public boolean isSectorPartiallyPopulated = false;
+	public boolean freeStart = false;
 
-    private ExerelinSetupData()
-    {
-	    // Empty constructor
-    }
-
-    public static ExerelinSetupData getInstance()
-    {
-	if(instance == null)
+	private ExerelinSetupData()
 	{
-	    instance = new ExerelinSetupData();
-	    ExerelinConfig.loadSettings();
+		// Empty constructor
 	}
 
-	    return instance;
-    }
-
-    public static void resetInstance()
-    {
+	public static ExerelinSetupData getInstance()
+	{
+	if(instance == null)
+	{
 		instance = new ExerelinSetupData();
-    }
+		ExerelinConfig.loadSettings();
+	}
 
-    public String[] getPossibleFactions()
-    {
+		return instance;
+	}
+
+	public static void resetInstance()
+	{
+		instance = new ExerelinSetupData();
+	}
+
+	public String[] getPossibleFactions()
+	{
 		ArrayList possibleFactionsList = new ArrayList();
 	
 		// Add built in factions
@@ -81,108 +81,108 @@ public final class ExerelinSetupData
 		// Add modded factions
 		Collections.addAll(possibleFactionsList, this.getModdedFactionsList());
 		return (String[])possibleFactionsList.toArray(new String[possibleFactionsList.size()]);
-    }
+	}
 
-    public void resetAvailableFactions()
-    {
-	    availableFactions = null;
-    }
+	public void resetAvailableFactions()
+	{
+		availableFactions = null;
+	}
 
-    public String[] getAvailableFactions(SectorAPI sector)
-    {
-	    if (availableFactions == null)
-	    {
-		    String[] locPossibleFaction = this.getPossibleFactions();
-		    ArrayList confirmedFactions = new ArrayList(locPossibleFaction.length);
+	public String[] getAvailableFactions(SectorAPI sector)
+	{
+		if (availableFactions == null)
+		{
+			String[] locPossibleFaction = this.getPossibleFactions();
+			ArrayList confirmedFactions = new ArrayList(locPossibleFaction.length);
 
-		    if(!onlyRespawnStartingFactions)
-		    {
-			    for(int i = 0; i < locPossibleFaction.length; i = i + 1)
-			    {
-				    FactionAPI fac = sector.getFaction(locPossibleFaction[i]);
-				    if(fac != null)
-					    confirmedFactions.add(fac.getId());
-				    else
-					    log.warn("EXERELIN ERROR: Couldn't determine faction for:" + locPossibleFaction[i]);
-			    }
-			    availableFactions = (String[])confirmedFactions.toArray( new String[confirmedFactions.size()] );
-		    }
-		    else
-		    {
-			    ExerelinUtils.shuffleStringArray(locPossibleFaction);
+			if(!onlyRespawnStartingFactions)
+			{
+				for(int i = 0; i < locPossibleFaction.length; i = i + 1)
+				{
+					FactionAPI fac = sector.getFaction(locPossibleFaction[i]);
+					if(fac != null)
+						confirmedFactions.add(fac.getId());
+					else
+						log.warn("EXERELIN ERROR: Couldn't determine faction for:" + locPossibleFaction[i]);
+				}
+				availableFactions = (String[])confirmedFactions.toArray( new String[confirmedFactions.size()] );
+			}
+			else
+			{
+				ExerelinUtils.shuffleStringArray(locPossibleFaction);
 
-			    int i = 0;
-			    while(confirmedFactions.size() < Math.min(this.numStartFactions, locPossibleFaction.length - 1))
-			    {
-				    if(locPossibleFaction[i].equalsIgnoreCase(PlayerFactionStore.getPlayerFactionId()))
-				    {
-					    i = i + 1;
-					    continue;
-				    }
+				int i = 0;
+				while(confirmedFactions.size() < Math.min(this.numStartFactions, locPossibleFaction.length - 1))
+				{
+					if(locPossibleFaction[i].equalsIgnoreCase(PlayerFactionStore.getPlayerFactionId()))
+					{
+						i = i + 1;
+						continue;
+					}
 
-				    FactionAPI fac = sector.getFaction(locPossibleFaction[i]);
-				    if(fac != null)
-				    {
-					    confirmedFactions.add(fac.getId());
-				    }
-				    else
-					    log.warn("EXERELIN ERROR: Couldn't determine faction for:" + locPossibleFaction[i]);
+					FactionAPI fac = sector.getFaction(locPossibleFaction[i]);
+					if(fac != null)
+					{
+						confirmedFactions.add(fac.getId());
+					}
+					else
+						log.warn("EXERELIN ERROR: Couldn't determine faction for:" + locPossibleFaction[i]);
 
-				    i = i + 1;
-			    }
-			    confirmedFactions.add(PlayerFactionStore.getPlayerFactionId());
+					i = i + 1;
+				}
+				confirmedFactions.add(PlayerFactionStore.getPlayerFactionId());
 
-			    availableFactions = (String[])confirmedFactions.toArray( new String[confirmedFactions.size()] );
-		    }
-	    }
-	    return availableFactions;
-    }
+				availableFactions = (String[])confirmedFactions.toArray( new String[confirmedFactions.size()] );
+			}
+		}
+		return availableFactions;
+	}
 
-    public String[] getModdedFactionsList()
-    {
+	public String[] getModdedFactionsList()
+	{
 	log.info("EXERELIN: Getting modded factions");
 	ArrayList possibleModdedFactionList = new ArrayList();
 
 	for(int i = 0; i < ExerelinConfig.exerelinFactionConfigs.size(); i++)
 	{
-	    ExerelinFactionConfig exerelinFactionConfig = (ExerelinFactionConfig)ExerelinConfig.exerelinFactionConfigs.get(i);
-	    if(exerelinFactionConfig.playableFaction
-		    && isFactionInstalled(exerelinFactionConfig.factionId, exerelinFactionConfig.uniqueModClassName))
-	    {
+		ExerelinFactionConfig exerelinFactionConfig = (ExerelinFactionConfig)ExerelinConfig.exerelinFactionConfigs.get(i);
+		if(exerelinFactionConfig.playableFaction
+			&& isFactionInstalled(exerelinFactionConfig.factionId, exerelinFactionConfig.uniqueModClassName))
+		{
 			possibleModdedFactionList.add(exerelinFactionConfig.factionId);
-	    }
+		}
 	}
-        return (String[])possibleModdedFactionList.toArray(new String[possibleModdedFactionList.size()]);
-    }
+		return (String[])possibleModdedFactionList.toArray(new String[possibleModdedFactionList.size()]);
+	}
 
-    public String[] getBuiltInFactionsList()
-    {
-        ArrayList possibleBuiltInFactionList = new ArrayList();
+	public String[] getBuiltInFactionsList()
+	{
+		ArrayList possibleBuiltInFactionList = new ArrayList();
 
-        for(int i = 0; i < ExerelinConfig.exerelinFactionConfigs.size(); i++)
-        {
-            ExerelinFactionConfig exerelinFactionConfig = (ExerelinFactionConfig)ExerelinConfig.exerelinFactionConfigs.get(i);
-            if(exerelinFactionConfig.playableFaction && exerelinFactionConfig.uniqueModClassName.equalsIgnoreCase(""))
-            {
-                    possibleBuiltInFactionList.add(exerelinFactionConfig.factionId);
-            }
-        }
+		for(int i = 0; i < ExerelinConfig.exerelinFactionConfigs.size(); i++)
+		{
+			ExerelinFactionConfig exerelinFactionConfig = (ExerelinFactionConfig)ExerelinConfig.exerelinFactionConfigs.get(i);
+			if(exerelinFactionConfig.playableFaction && exerelinFactionConfig.uniqueModClassName.equalsIgnoreCase(""))
+			{
+				possibleBuiltInFactionList.add(exerelinFactionConfig.factionId);
+			}
+		}
 
-        return (String[])possibleBuiltInFactionList.toArray(new String[possibleBuiltInFactionList.size()]);
-    }
+		return (String[])possibleBuiltInFactionList.toArray(new String[possibleBuiltInFactionList.size()]);
+	}
 
-    public boolean isFactionInstalled(String factionId, String factionSpecficClassName)
-    {
-	    try
-	    {
-		    Global.getSettings().getScriptClassLoader().loadClass(factionSpecficClassName);
-		    log.info("Loaded " + factionId);
-		    return true;
-	    }
-	    catch (ClassNotFoundException ex)
-	    {
-		    log.info("Skipped " + factionId);
-		    return false;
-	    }
-    }
+	public boolean isFactionInstalled(String factionId, String factionSpecficClassName)
+	{
+		try
+		{
+			Global.getSettings().getScriptClassLoader().loadClass(factionSpecficClassName);
+			log.info("Loaded " + factionId);
+			return true;
+		}
+		catch (ClassNotFoundException ex)
+		{
+			log.info("Skipped " + factionId);
+			return false;
+		}
+	}
 }
