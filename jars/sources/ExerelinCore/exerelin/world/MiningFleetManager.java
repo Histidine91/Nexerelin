5aa6de64f8d74616db2771d7696547f30e51eeb9
L.J. Lim <kingraptorowns@gmail.com>
2015-07-19 00:04:48 +0800

    MiningFleetManager: consistent indents

--- jars/sources/ExerelinCore/exerelin/world/MiningFleetManager.java
+++ jars/sources/ExerelinCore/exerelin/world/MiningFleetManager.java
@@ -35,30 +35,30 @@ import org.apache.log4j.Logger;
  */
 public class MiningFleetManager extends BaseCampaignEventListener implements EveryFrameScript
 {
-    public static final String MANAGER_MAP_KEY = "exerelin_miningFleetManager";
-        
-    public static Logger log = Global.getLogger(MiningFleetManager.class);
-    protected static final float POINT_INCREMENT_PER_DAY = 4f;
-    protected static final float MARKET_STABILITY_DIVISOR = 5f;
+	public static final String MANAGER_MAP_KEY = "exerelin_miningFleetManager";
+		
+	public static Logger log = Global.getLogger(MiningFleetManager.class);
+	protected static final float POINT_INCREMENT_PER_DAY = 4f;
+	protected static final float MARKET_STABILITY_DIVISOR = 5f;
 	protected static final float POINTS_TO_SPAWN = 100f;
 	
-    protected final List<MiningFleetData> activeFleets = new LinkedList();
-    protected HashMap<String, Float> spawnCounter = new HashMap<>();
-    
-    private final IntervalUtil tracker;
-    
-    protected float daysElapsed = 0;
-    
-    private static MiningFleetManager miningFleetManager;
-  
-    public MiningFleetManager()
-    {
-        super(true);
-        this.tracker = new IntervalUtil(1, 1);
-    }
+	protected final List<MiningFleetData> activeFleets = new LinkedList();
+	protected HashMap<String, Float> spawnCounter = new HashMap<>();
+	
+	private final IntervalUtil tracker;
+	
+	protected float daysElapsed = 0;
+	
+	private static MiningFleetManager miningFleetManager;
+	
+	public MiningFleetManager()
+	{
+		super(true);
+		this.tracker = new IntervalUtil(1, 1);
+	}
 	
 	public void spawnMiningFleet(MarketAPI origin)
-    {
+	{
 		log.info("Trying mining fleet for market " + origin.getName());
 		SectorEntityToken target = null;
 		LocationAPI loc = origin.getContainingLocation();
@@ -66,11 +66,11 @@ public class MiningFleetManager extends BaseCampaignEventListener implements Eve
 		
 		FactionAPI faction = origin.getFaction();
 		int marketSize = origin.getSize();
-        int maxFP = (int)(Math.pow(marketSize, 1.5f) * 5);
-        
-        float qf = origin.getShipQualityFactor();
-        //qf = Math.max(qf, 0.7f);
-        
+		int maxFP = (int)(Math.pow(marketSize, 1.5f) * 5);
+		
+		float qf = origin.getShipQualityFactor();
+		//qf = Math.max(qf, 0.7f);
+		
 		boolean isGasMiningFleet = false;
 		boolean hasOreFacilities = origin.hasCondition(Conditions.ORE_COMPLEX) || origin.hasCondition(Conditions.ORE_REFINING_COMPLEX) 
 				|| faction.getId().equals("spire") || faction.getId().equals("darkspire");
@@ -130,17 +130,17 @@ public class MiningFleetManager extends BaseCampaignEventListener implements Eve
 			return;
 		}
 		
-        String name = StringHelper.getString("exerelin_fleets", "miningFleetName");
-        ExerelinFactionConfig factionConfig = ExerelinConfig.getExerelinFactionConfig(origin.getFactionId());
-        if (factionConfig != null)
-        {
-            name = factionConfig.asteroidMiningFleetName;
-        }
-        if (maxFP < 50) name = StringHelper.getString("exerelin_fleets", "miningFleetPrefixSmall") + " " + name;
-        else if (maxFP > 100) name = StringHelper.getString("exerelin_fleets", "miningFleetPrefixLarge") + " " + name;
+		String name = StringHelper.getString("exerelin_fleets", "miningFleetName");
+		ExerelinFactionConfig factionConfig = ExerelinConfig.getExerelinFactionConfig(origin.getFactionId());
+		if (factionConfig != null)
+		{
+			name = factionConfig.asteroidMiningFleetName;
+		}
+		if (maxFP < 50) name = StringHelper.getString("exerelin_fleets", "miningFleetPrefixSmall") + " " + name;
+		else if (maxFP > 100) name = StringHelper.getString("exerelin_fleets", "miningFleetPrefixLarge") + " " + name;
 		
 		//log.info("Trying to create mining fleet of size " + maxFP + ", target " + target.getName());
-        CampaignFleetAPI fleet = FleetFactory.createGenericFleet(origin.getFactionId(), name, qf, maxFP/3);
+		CampaignFleetAPI fleet = FleetFactory.createGenericFleet(origin.getFactionId(), name, qf, maxFP/3);
 		
 		float minerFP = maxFP - fleet.getFleetPoints();
 		while (minerFP > 0)
@@ -158,140 +158,140 @@ public class MiningFleetManager extends BaseCampaignEventListener implements Eve
 			}
 			minerFP = maxFP - fleet.getFleetPoints();
 		}
-             
-        fleet.getMemoryWithoutUpdate().set("$fleetType", "exerelinMiningFleet");
-        fleet.getMemoryWithoutUpdate().set("$maxFP", maxFP);
-        fleet.getMemoryWithoutUpdate().set("$originMarket", origin);
-        
+			 
+		fleet.getMemoryWithoutUpdate().set("$fleetType", "exerelinMiningFleet");
+		fleet.getMemoryWithoutUpdate().set("$maxFP", maxFP);
+		fleet.getMemoryWithoutUpdate().set("$originMarket", origin);
+		
 		float miningStrength = MiningHelper.getFleetMiningStrength(fleet);
 		
-        SectorEntityToken entity = origin.getPrimaryEntity();
-        entity.getContainingLocation().addEntity(fleet);
-        fleet.setLocation(entity.getLocation().x, entity.getLocation().y);
-        
-        MiningFleetData data = new MiningFleetData(fleet);
-        data.startingFleetPoints = fleet.getFleetPoints();
-        data.sourceMarket = origin;
-        data.source = origin.getPrimaryEntity();
-        data.target = target;
+		SectorEntityToken entity = origin.getPrimaryEntity();
+		entity.getContainingLocation().addEntity(fleet);
+		fleet.setLocation(entity.getLocation().x, entity.getLocation().y);
+		
+		MiningFleetData data = new MiningFleetData(fleet);
+		data.startingFleetPoints = fleet.getFleetPoints();
+		data.sourceMarket = origin;
+		data.source = origin.getPrimaryEntity();
+		data.target = target;
 		data.miningStrength = miningStrength;
-        this.activeFleets.add(data);
-        
-        MiningFleetAI ai = new MiningFleetAI(fleet, data);
-        fleet.addScript(ai);
-        log.info("\tSpawned " + fleet.getNameWithFaction() + " of size " + maxFP);
-    }
+		this.activeFleets.add(data);
+		
+		MiningFleetAI ai = new MiningFleetAI(fleet, data);
+		fleet.addScript(ai);
+		log.info("\tSpawned " + fleet.getNameWithFaction() + " of size " + maxFP);
+	}
 	
 	public void updateMiningFleetPoints(float days)
-    {
-        // prevents NPE of unknown origin
-        if (Global.getSector() == null || Global.getSector().getEconomy() == null)
-            return;
-        
-        List<MarketAPI> markets = Global.getSector().getEconomy().getMarketsCopy();
-        for(MarketAPI market:markets)
-        {
+	{
+		// prevents NPE of unknown origin
+		if (Global.getSector() == null || Global.getSector().getEconomy() == null)
+			return;
+		
+		List<MarketAPI> markets = Global.getSector().getEconomy().getMarketsCopy();
+		for(MarketAPI market:markets)
+		{
 			if (ExerelinUtilsFaction.isPirateOrTemplarFaction(market.getFactionId()))
 				continue;
 			
-            if (!spawnCounter.containsKey(market.getId()))
-                spawnCounter.put(market.getId(), 0f);
-            
-            float baseIncrement = (0.5f + (market.getStabilityValue()/MARKET_STABILITY_DIVISOR));
-            float increment = baseIncrement;
-            //if (market.hasCondition("regional_capital")) increment += baseIncrement * 0.1f;
-            if (market.hasCondition(Conditions.SPACEPORT)) increment += baseIncrement * 0.25f;
+			if (!spawnCounter.containsKey(market.getId()))
+				spawnCounter.put(market.getId(), 0f);
+			
+			float baseIncrement = (0.5f + (market.getStabilityValue()/MARKET_STABILITY_DIVISOR));
+			float increment = baseIncrement;
+			//if (market.hasCondition("regional_capital")) increment += baseIncrement * 0.1f;
+			if (market.hasCondition(Conditions.SPACEPORT)) increment += baseIncrement * 0.25f;
 			if (market.hasCondition(Conditions.ORBITAL_STATION)) increment += baseIncrement * 0.25f;
-            
-            ExerelinFactionConfig factionConfig = ExerelinConfig.getExerelinFactionConfig(market.getFactionId());
-            if (factionConfig != null)
-            {
-                increment += baseIncrement * factionConfig.responseFleetSizeMod;
-            }
-            
-            increment = increment * POINT_INCREMENT_PER_DAY * days;
-            float newValue = spawnCounter.get(market.getId()) + increment;
+			
+			ExerelinFactionConfig factionConfig = ExerelinConfig.getExerelinFactionConfig(market.getFactionId());
+			if (factionConfig != null)
+			{
+				increment += baseIncrement * factionConfig.responseFleetSizeMod;
+			}
+			
+			increment = increment * POINT_INCREMENT_PER_DAY * days;
+			float newValue = spawnCounter.get(market.getId()) + increment;
 			
 			if (newValue > POINTS_TO_SPAWN)
 			{
 				newValue -= POINTS_TO_SPAWN;
 				spawnMiningFleet(market);
 			}
-            
-            spawnCounter.put(market.getId(), newValue);
-        }
-    }
+			
+			spawnCounter.put(market.getId(), newValue);
+		}
+	}
   
-    @Override
-    public void advance(float amount)
-    {
-        float days = Global.getSector().getClock().convertToDays(amount);
-            
-        this.tracker.advance(days);
-        if (!this.tracker.intervalElapsed()) {
-            return;
-        }
-        List<MiningFleetData> remove = new LinkedList();
-        for (MiningFleetData data : this.activeFleets) {
-            if ((data.fleet.getContainingLocation() == null) || (!data.fleet.getContainingLocation().getFleets().contains(data.fleet)) || (!data.fleet.isAlive())) {
-                remove.add(data);
-            }
-        }
-        this.activeFleets.removeAll(remove);
-    
-        updateMiningFleetPoints(days);
-    }
-    
-    public static MiningFleetManager create()
-    {
-        Map<String, Object> data = Global.getSector().getPersistentData();
-        miningFleetManager = (MiningFleetManager)data.get(MANAGER_MAP_KEY);
-        if (miningFleetManager != null)
-            return miningFleetManager;
-        
-        miningFleetManager = new MiningFleetManager();
-        data.put(MANAGER_MAP_KEY, miningFleetManager);
-        return miningFleetManager;
-    }
+	@Override
+	public void advance(float amount)
+	{
+		float days = Global.getSector().getClock().convertToDays(amount);
+			
+		this.tracker.advance(days);
+		if (!this.tracker.intervalElapsed()) {
+			return;
+		}
+		List<MiningFleetData> remove = new LinkedList();
+		for (MiningFleetData data : this.activeFleets) {
+			if ((data.fleet.getContainingLocation() == null) || (!data.fleet.getContainingLocation().getFleets().contains(data.fleet)) || (!data.fleet.isAlive())) {
+				remove.add(data);
+			}
+		}
+		this.activeFleets.removeAll(remove);
+	
+		updateMiningFleetPoints(days);
+	}
+	
+	public static MiningFleetManager create()
+	{
+		Map<String, Object> data = Global.getSector().getPersistentData();
+		miningFleetManager = (MiningFleetManager)data.get(MANAGER_MAP_KEY);
+		if (miningFleetManager != null)
+			return miningFleetManager;
+		
+		miningFleetManager = new MiningFleetManager();
+		data.put(MANAGER_MAP_KEY, miningFleetManager);
+		return miningFleetManager;
+	}
   
-    @Override
-    public boolean isDone()
-    {
-        return false;
-    }
+	@Override
+	public boolean isDone()
+	{
+		return false;
+	}
   
-    @Override
-    public void reportFleetDespawned(CampaignFleetAPI fleet, CampaignEventListener.FleetDespawnReason reason, Object param)
-    {
-        super.reportFleetDespawned(fleet, reason, param);
-        for (MiningFleetData data : this.activeFleets) {
-            if (data.fleet == fleet)
-            {
-                this.activeFleets.remove(data);
-                break;
-            }
-        }
-    }
-  
-    @Override
-    public boolean runWhilePaused()
-    {
-        return false;
-    }
-  
-    public static class MiningFleetData
-    {
-        public CampaignFleetAPI fleet;
-        public SectorEntityToken source;
-        public SectorEntityToken target;
-        public MarketAPI sourceMarket;
-        public float startingFleetPoints = 0.0F;
+	@Override
+	public void reportFleetDespawned(CampaignFleetAPI fleet, CampaignEventListener.FleetDespawnReason reason, Object param)
+	{
+		super.reportFleetDespawned(fleet, reason, param);
+		for (MiningFleetData data : this.activeFleets) {
+			if (data.fleet == fleet)
+			{
+				this.activeFleets.remove(data);
+				break;
+			}
+		}
+	}
+	
+	@Override
+	public boolean runWhilePaused()
+	{
+		return false;
+	}
+	
+	public static class MiningFleetData
+	{
+		public CampaignFleetAPI fleet;
+		public SectorEntityToken source;
+		public SectorEntityToken target;
+		public MarketAPI sourceMarket;
+		public float startingFleetPoints = 0.0F;
 		public float miningStrength = 0;
-        public boolean noWait = false;
-    
-        public MiningFleetData(CampaignFleetAPI fleet)
-        {
-            this.fleet = fleet;
-        }
-    }
+		public boolean noWait = false;
+	
+		public MiningFleetData(CampaignFleetAPI fleet)
+		{
+			this.fleet = fleet;
+		}
+	}
 }
