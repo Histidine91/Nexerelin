f1ae2c522820361e926b052f30c2f4b6ff0c9e3e
L.J. Lim <kingraptorowns@gmail.com>
2015-09-17 20:22:25 +0800

    Military base market condition removes the military submarket it adds, if on a Templar market

--- jars/sources/ExerelinCore/data/scripts/campaign/econ/ExerelinMilitaryBase.java
+++ jars/sources/ExerelinCore/data/scripts/campaign/econ/ExerelinMilitaryBase.java
@@ -1,11 +1,16 @@
 package data.scripts.campaign.econ;
 
 import com.fs.starfarer.api.impl.campaign.econ.MilitaryBase;
+import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
 
 public class ExerelinMilitaryBase extends MilitaryBase {
 
 	public void apply(String id) {
 		super.apply(id);
+		if (this.market.getFactionId().equals("templars"))
+		{
+			market.removeSubmarket(Submarkets.GENERIC_MILITARY);
+		}
 		market.getCommodityData("agent").getSupply().modifyFlat(id, 2);
 		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 1);
 	}
