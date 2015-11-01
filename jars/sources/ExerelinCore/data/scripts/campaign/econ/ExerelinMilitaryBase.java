5c0bbceddd7573ae97863fe67f997c01463caf72
L.J. Lim <kingraptorowns@gmail.com>
2015-11-01 15:02:08 +0800

    Military base makes 25% more marines than in vanilla

--- jars/sources/ExerelinCore/data/scripts/campaign/econ/ExerelinMilitaryBase.java
+++ jars/sources/ExerelinCore/data/scripts/campaign/econ/ExerelinMilitaryBase.java
@@ -1,16 +1,20 @@
 package data.scripts.campaign.econ;
 
+import com.fs.starfarer.api.impl.campaign.econ.ConditionData;
 import com.fs.starfarer.api.impl.campaign.econ.MilitaryBase;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
 
 public class ExerelinMilitaryBase extends MilitaryBase {
-
+	public static float EXTRA_MARINES_MULT = 1.25f;	// hax
+	
 	public void apply(String id) {
 		super.apply(id);
 		if (this.market.getFactionId().equals("templars"))
 		{
 			market.removeSubmarket(Submarkets.GENERIC_MILITARY);
 		}
+		market.getCommodityData(Commodities.MARINES).getSupply().modifyFlat(id, ConditionData.MILITARY_BASE_MARINES_SUPPLY * EXTRA_MARINES_MULT);
 		market.getCommodityData("agent").getSupply().modifyFlat(id, 2);
 		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 1);
 	}
