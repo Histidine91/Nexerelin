39de517e43a9d96ed771cb87889b20ef199e7b24
L.J. Lim <kingraptorowns@gmail.com>
2015-12-05 12:34:20 +0800

    Remove fuel non-consuming demand override from Military Base (no longer needed)

--- jars/sources/ExerelinCore/data/scripts/campaign/econ/ExerelinMilitaryBase.java
+++ jars/sources/ExerelinCore/data/scripts/campaign/econ/ExerelinMilitaryBase.java
@@ -7,7 +7,6 @@ import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
 
 public class ExerelinMilitaryBase extends MilitaryBase {
 	public static final float EXTRA_MARINES_MULT = 1.4f;	// hax
-	public static final float FUEL_NON_CONSUMING_FRACTION = 0.5f;	// more hax	// remove in 0.7.1a
 	
 	public void apply(String id) {
 		super.apply(id);
@@ -18,8 +17,6 @@ public class ExerelinMilitaryBase extends MilitaryBase {
 		market.getCommodityData(Commodities.MARINES).getSupply().modifyFlat(id, ConditionData.MILITARY_BASE_MARINES_SUPPLY * EXTRA_MARINES_MULT);
 		market.getCommodityData("agent").getSupply().modifyFlat(id, 2);
 		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 1);
-		
-		market.getDemand(Commodities.FUEL).getNonConsumingDemand().modifyFlat(id, ConditionData.MILITARY_BASE_FUEL * FUEL_NON_CONSUMING_FRACTION);
 	}
 
 	public void unapply(String id) {
