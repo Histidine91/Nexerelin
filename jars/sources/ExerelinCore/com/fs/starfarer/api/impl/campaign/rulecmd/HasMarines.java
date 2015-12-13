c2493ff61ce0c00df6ed5d22d9089b4893c881fc
L.J. Lim <kingraptorowns@gmail.com>
2015-12-13 18:08:39 +0800

    HasMarines takes a number arg

--- jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/HasMarines.java
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/HasMarines.java
@@ -9,7 +9,8 @@ import java.util.Map;
 
 
 public class HasMarines extends BaseCommandPlugin {
-		public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
-				return (Global.getSector().getPlayerFleet().getCargo().getMarines() > 0);
-        }
+	public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+		int num = (int)params.get(0).getFloat(memoryMap);
+		return (Global.getSector().getPlayerFleet().getCargo().getMarines() >= num);
+	}
 }
