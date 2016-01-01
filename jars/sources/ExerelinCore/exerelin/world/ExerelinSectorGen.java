436c6a6529266f90c14c4fcffcd2664c43a61fb9
L.J. Lim <kingraptorowns@gmail.com>
2016-01-01 09:38:51 +0800

    Fix broken nebula/hyperspace setup at generation

--- jars/sources/ExerelinCore/exerelin/world/ExerelinSectorGen.java
+++ jars/sources/ExerelinCore/exerelin/world/ExerelinSectorGen.java
@@ -234,7 +234,7 @@ public class ExerelinSectorGen implements SectorGeneratorPlugin
 		nebulaMaps.add("eos_nebula.png");
 		nebulaMaps.add("valhalla_nebula.png");
 		nebulaMaps.add("hybrasil_nebula.png");
-		nebulaMaps.add("Nexerelin/gemstone_nebula.png");
+		nebulaMaps.add("nexerelin/gemstone_nebula.png");
 		
 		if (ExerelinUtilsFaction.doesFactionExist("blackrock_driveyards"))
 		{
@@ -716,6 +716,7 @@ public class ExerelinSectorGen implements SectorGeneratorPlugin
 		
 		log.info("Loading backgrounds");
 		loadBackgrounds();
+		loadNebulaMaps();
 		
 		log.info("Resetting vars");
 		resetVars();
@@ -781,7 +782,7 @@ public class ExerelinSectorGen implements SectorGeneratorPlugin
 		String hyperMap = "data/campaign/terrain/hyperspace_map.png";
 		if (!corvusMode)
 		{
-			hyperMap = "data/campaign/terrain/Nexerelin/hyperspace_map_rot.png";
+			hyperMap = "data/campaign/terrain/nexerelin/hyperspace_map_rot.png";
 		}
 		SectorEntityToken deep_hyperspace = Misc.addNebulaFromPNG(hyperMap,
 			  0, 0, // center of nebula
