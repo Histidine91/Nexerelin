9b42f9ab4b6bf2b3c33d15dcdd68e1a23bea464f
L.J. Lim <kingraptorowns@gmail.com>
2016-03-12 00:06:11 +0800

    Fix commissions not being removed properly on low reputation

--- jars/sources/ExerelinCore/exerelin/campaign/events/ExerelinFactionCommissionMissionEvent.java
+++ jars/sources/ExerelinCore/exerelin/campaign/events/ExerelinFactionCommissionMissionEvent.java
@@ -1,6 +1,10 @@
 package exerelin.campaign.events;
 
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.BaseOnMessageDeliveryScript;
+import com.fs.starfarer.api.campaign.RepLevel;
+import com.fs.starfarer.api.campaign.comm.CommMessageAPI;
+import com.fs.starfarer.api.campaign.comm.MessagePriority;
 import com.fs.starfarer.api.impl.campaign.ids.MemFlags;
 import com.fs.starfarer.api.impl.campaign.missions.FactionCommissionMissionEvent;
 
@@ -15,6 +19,27 @@ public class ExerelinFactionCommissionMissionEvent extends FactionCommissionMiss
 		
 		Global.getSector().getCharacterData().getMemoryWithoutUpdate().unset(MemFlags.FCM_FACTION);
 		Global.getSector().getCharacterData().getMemoryWithoutUpdate().unset(MemFlags.FCM_EVENT);
+	}
+	
+	// same as vanilla
+	@Override
+	public void advance(float amount) {
+		if (!isEventStarted()) return;
+		if (isDone()) return;
+		
+		//System.out.println("Registered: " + Global.getSector().getAllListeners().contains(this));
+		float days = Global.getSector().getClock().convertToDays(amount);
+		RepLevel level = faction.getRelToPlayer().getLevel();
+		if (!level.isAtWorst(RepLevel.NEUTRAL)) {
+			endEvent();
+			Global.getSector().reportEventStage(this, "annul", findMessageSender(),
+					MessagePriority.ENSURE_DELIVERY,
+					new BaseOnMessageDeliveryScript() {
+				public void beforeDelivery(CommMessageAPI message) {
+					
+				}
+			});
+		}
 	}
 	
 	@Override
