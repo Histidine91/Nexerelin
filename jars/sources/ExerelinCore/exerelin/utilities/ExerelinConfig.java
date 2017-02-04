edf569245d28b9078a4f5f050c3e74d790200334
L.J. Lim <kingraptorowns@gmail.com>
2017-02-04 23:54:57 +0800

    Agent, prisoner nerf * Agent raise relations effect range 0.8-0.14 (was 0.9-0.14) * Prisoner drop rate per 10 FP reduced 0.04 -> 0.025

--- jars/sources/ExerelinCore/exerelin/utilities/ExerelinConfig.java
+++ jars/sources/ExerelinCore/exerelin/utilities/ExerelinConfig.java
@@ -46,7 +46,7 @@ public class ExerelinConfig
     public static float prisonerBaseSlaveValue = 4000f;
     public static float prisonerSlaveValueIncrementPerLevel = 400f;
     public static float prisonerSlaveRepValue = -0.02f;
-    public static float prisonerLootChancePer10Fp = 0.04f;
+    public static float prisonerLootChancePer10Fp = 0.025f;
     
     public static float crewLootMult = 0.02f;
 
