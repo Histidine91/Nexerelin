ecbaadaa086ab2545b55c0c73deb139e52a04d24
L.J. Lim <kingraptorowns@gmail.com>
2017-03-22 20:13:55 +0800

    Fix SetRelationship not working when player-aligned faction is passed as an arg

--- jars/sources/ExerelinCore/data/console/commands/SetRelationship.java
+++ jars/sources/ExerelinCore/data/console/commands/SetRelationship.java
@@ -1,16 +1,28 @@
 package data.console.commands;
 
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
 import exerelin.utilities.ExerelinUtilsReputation;
 
 public class SetRelationship extends org.lazywizard.console.commands.SetRelationship {
 
-    @Override
-    public CommandResult runCommand(String args, CommandContext context) {
-        CommandResult result = super.runCommand(args, context);
+	@Override
+	public CommandResult runCommand(String args, CommandContext context) {
+		CommandResult result = super.runCommand(args, context);
 		if (result == CommandResult.SUCCESS)
 		{
-			ExerelinUtilsReputation.syncFactionRelationshipsToPlayer();
+			String[] tmp = args.split(" ");
+			String factionId1 = tmp[0];
+			String factionId2 = tmp[1];
+			//String alignedFactionId = PlayerFactionStore.getPlayerFactionId();
+			if (tmp.length == 2)
+			{
+				factionId2 = Factions.PLAYER;
+			}
+			if (factionId1.equals(Factions.PLAYER) || factionId2.equals(Factions.PLAYER))
+				ExerelinUtilsReputation.syncFactionRelationshipsToPlayer();
+			else// if (factionId1.equals(alignedFactionId) || factionId2.equals(Factions.PLAYER))
+				ExerelinUtilsReputation.syncFactionRelationshipsToPlayer();
 		}
 		return result;
-    }
+	}
 }
