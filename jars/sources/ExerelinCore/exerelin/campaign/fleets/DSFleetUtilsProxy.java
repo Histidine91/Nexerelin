627888b25fb00c072907c09c335703ac753abfa5
L.J. Lim <kingraptorowns@gmail.com>
2017-04-11 22:39:38 +0800

    DS compatibility update for real

--- jars/sources/ExerelinCore/exerelin/campaign/fleets/DSFleetUtilsProxy.java
+++ jars/sources/ExerelinCore/exerelin/campaign/fleets/DSFleetUtilsProxy.java
@@ -6,9 +6,9 @@ import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.impl.campaign.fleets.FleetParams;
 import data.scripts.campaign.DS_FleetFactory;
 import data.scripts.campaign.fleets.DS_FleetInjector;
-import static data.scripts.campaign.fleets.DS_FleetInjector.getArchetypeWeights;
 import static data.scripts.campaign.fleets.DS_FleetInjector.randomizeVariants;
-import data.scripts.variants.DS_VariantRandomizer;
+import data.scripts.util.DS_Defs;
+import static data.scripts.util.DS_Util.getArchetypeWeights;
 
 public class DSFleetUtilsProxy {
 	
@@ -25,32 +25,32 @@ public class DSFleetUtilsProxy {
 	public static void injectFleet(CampaignFleetAPI fleet, MarketAPI market, Float stability, Float qualityFactor, String type) {
 		String factionId = fleet.getFaction().getId();
         
-        DS_VariantRandomizer.Archetype theme = DS_FleetInjector.pickTheme(factionId);
+        DS_Defs.Archetype theme = DS_FleetInjector.pickTheme(factionId);
         DS_FleetInjector.setThemeName(fleet, theme);
         
         switch (type)
         {
             case "exerelinInvasionFleet":
             case "exerelinRespawnFleet":
-                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_FleetInjector.FleetStyle.ELITE, factionId), DS_FleetInjector.CommanderType.ELITE);
-                DS_FleetInjector.levelFleet(fleet, DS_FleetInjector.CrewType.MILITARY, factionId, DS_FleetInjector.CommanderType.ELITE);
+                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_Defs.FleetStyle.ELITE, factionId), DS_Defs.CommanderType.ELITE);
+                DS_FleetInjector.levelFleet(fleet, DS_Defs.CrewType.MILITARY, factionId, DS_Defs.CommanderType.ELITE);
                 break;
             case "exerelinInvasionSupportFleet":
             case "exerelinDefenceFleet":
-                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_FleetInjector.FleetStyle.MILITARY, factionId), DS_FleetInjector.CommanderType.MILITARY);
-                DS_FleetInjector.levelFleet(fleet, DS_FleetInjector.CrewType.MILITARY, factionId, DS_FleetInjector.CommanderType.MILITARY);
+                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_Defs.FleetStyle.MILITARY, factionId), DS_Defs.CommanderType.MILITARY);
+                DS_FleetInjector.levelFleet(fleet, DS_Defs.CrewType.MILITARY, factionId, DS_Defs.CommanderType.MILITARY);
                 break;
             case "exerelinResponseFleet":
-                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_FleetInjector.FleetStyle.MILITARY, factionId), DS_FleetInjector.CommanderType.MILITARY);
-                DS_FleetInjector.levelFleet(fleet, DS_FleetInjector.CrewType.MILITARY, factionId, DS_FleetInjector.CommanderType.ELITE);
+                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_Defs.FleetStyle.MILITARY, factionId), DS_Defs.CommanderType.MILITARY);
+                DS_FleetInjector.levelFleet(fleet, DS_Defs.CrewType.MILITARY, factionId, DS_Defs.CommanderType.ELITE);
                 break;  
             case "exerelinMiningFleet":
-                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_FleetInjector.FleetStyle.CIVILIAN, factionId), DS_FleetInjector.CommanderType.CIVILIAN);
-                DS_FleetInjector.levelFleet(fleet, DS_FleetInjector.CrewType.CIVILIAN, factionId, DS_FleetInjector.CommanderType.CIVILIAN);
+                randomizeVariants(fleet, factionId, qualityFactor, theme, getArchetypeWeights(DS_Defs.FleetStyle.CIVILIAN, factionId), DS_Defs.CommanderType.CIVILIAN);
+                DS_FleetInjector.levelFleet(fleet, DS_Defs.CrewType.CIVILIAN, factionId, DS_Defs.CommanderType.CIVILIAN);
                 break;
             default:    // fallback taken from SS+
-                randomizeVariants(fleet, factionId, qualityFactor, null, getArchetypeWeights(DS_FleetInjector.FleetStyle.STANDARD, factionId), DS_FleetInjector.CommanderType.STANDARD);
-                DS_FleetInjector.levelFleet(fleet, DS_FleetInjector.CrewType.STANDARD, factionId, DS_FleetInjector.CommanderType.STANDARD);
+                randomizeVariants(fleet, factionId, qualityFactor, null, getArchetypeWeights(DS_Defs.FleetStyle.STANDARD, factionId), DS_Defs.CommanderType.STANDARD);
+                DS_FleetInjector.levelFleet(fleet, DS_Defs.CrewType.STANDARD, factionId, DS_Defs.CommanderType.STANDARD);
         }
         DS_FleetFactory.finishFleetNonIntrusive(fleet, factionId);
 	}
