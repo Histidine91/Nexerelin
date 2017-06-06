1fc13407edb829a56344d226dadf537563e58bd6
L.J. Lim <kingraptorowns@gmail.com>
2017-06-06 21:17:00 +0800

    Fix agent/saboteur availability on markets

--- jars/sources/ExerelinCore/exerelin/campaign/econ/ExerelinMilitaryBase.java
+++ jars/sources/ExerelinCore/exerelin/campaign/econ/ExerelinMilitaryBase.java
@@ -6,7 +6,7 @@ import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
 
 public class ExerelinMilitaryBase extends MilitaryBase {
-	public static final float EXTRA_MARINES_MULT = 1.5f;	// hax
+	public static final float EXTRA_MARINES_MULT = 1.4f;	// hax
 	
 	public void apply(String id) {
 		super.apply(id);
@@ -15,8 +15,12 @@ public class ExerelinMilitaryBase extends MilitaryBase {
 			market.removeSubmarket(Submarkets.GENERIC_MILITARY);
 		}
 		market.getCommodityData(Commodities.MARINES).getSupply().modifyFlat(id, ConditionData.MILITARY_BASE_MARINES_SUPPLY * EXTRA_MARINES_MULT);
-		market.getCommodityData("agent").getSupply().modifyFlat(id, 5f);
-		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 5f);
+		market.getCommodityData("agent").getSupply().modifyFlat(id, 16f);
+		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 16f);
+		market.getCommodityData(Commodities.MARINES).getDemand().getDemand().modifyFlat(id, 
+				ConditionData.MILITARY_BASE_MARINES_DEMAND * EXTRA_MARINES_MULT);
+		market.getCommodityData(Commodities.MARINES).getDemand().getNonConsumingDemand().modifyFlat(id, 
+				ConditionData.MILITARY_BASE_MARINES_DEMAND * ConditionData.CREW_MARINES_NON_CONSUMING_FRACTION * EXTRA_MARINES_MULT);
 	}
 
 	public void unapply(String id) {
