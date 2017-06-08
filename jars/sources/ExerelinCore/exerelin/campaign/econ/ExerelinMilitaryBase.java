628714e6fb9dc9185a55f4f2b03a766da9a2d503
L.J. Lim <kingraptorowns@gmail.com>
2017-06-08 22:51:17 +0800

    New agent/saboteur supply handling * Only available on worlds with military bases * Doesn't fill marine demand any more * Higher price Also fix prisoner handling in economy system

--- jars/sources/ExerelinCore/exerelin/campaign/econ/ExerelinMilitaryBase.java
+++ jars/sources/ExerelinCore/exerelin/campaign/econ/ExerelinMilitaryBase.java
@@ -7,6 +7,7 @@ import com.fs.starfarer.api.impl.campaign.ids.Submarkets;
 
 public class ExerelinMilitaryBase extends MilitaryBase {
 	public static final float EXTRA_MARINES_MULT = 1.4f;	// hax
+	public static final float AGENT_SUPPLY = 8f;
 	
 	public void apply(String id) {
 		super.apply(id);
@@ -15,18 +16,23 @@ public class ExerelinMilitaryBase extends MilitaryBase {
 			market.removeSubmarket(Submarkets.GENERIC_MILITARY);
 		}
 		market.getCommodityData(Commodities.MARINES).getSupply().modifyFlat(id, ConditionData.MILITARY_BASE_MARINES_SUPPLY * EXTRA_MARINES_MULT);
-		market.getCommodityData("agent").getSupply().modifyFlat(id, 16f);
-		market.getCommodityData("saboteur").getSupply().modifyFlat(id, 16f);
+		market.getCommodityData("agent").getSupply().modifyFlat(id, AGENT_SUPPLY);
+		market.getCommodityData("saboteur").getSupply().modifyFlat(id, AGENT_SUPPLY);
 		market.getCommodityData(Commodities.MARINES).getDemand().getDemand().modifyFlat(id, 
 				ConditionData.MILITARY_BASE_MARINES_DEMAND * EXTRA_MARINES_MULT);
 		market.getCommodityData(Commodities.MARINES).getDemand().getNonConsumingDemand().modifyFlat(id, 
 				ConditionData.MILITARY_BASE_MARINES_DEMAND * ConditionData.CREW_MARINES_NON_CONSUMING_FRACTION * EXTRA_MARINES_MULT);
+		market.getCommodityData("agent").getDemand().getDemand().modifyFlat(id, AGENT_SUPPLY * 2);
+		market.getCommodityData("agent").getDemand().getNonConsumingDemand().modifyFlat(id, AGENT_SUPPLY * 2
+				* ConditionData.CREW_MARINES_NON_CONSUMING_FRACTION);
 	}
 
 	public void unapply(String id) {
 		super.unapply(id);
 		market.getCommodityData("agent").getSupply().unmodify(id);
 		market.getCommodityData("saboteur").getSupply().unmodify(id);
+		market.getCommodityData("agent").getDemand().getDemand().unmodify(id);
+		market.getCommodityData("agent").getDemand().getNonConsumingDemand().unmodify(id);
 	}
 
 }
