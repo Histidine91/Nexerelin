2f7eacdb27a1d5ad9e0d8059d8282d6bee942884
L.J. Lim <kingraptorowns@gmail.com>
2018-03-05 21:46:21 +0800

    When turning in AI cores, display relationship limit if applicable

---
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/Nex_AICores.java
@@ -0,0 +1,141 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CargoAPI;
+import com.fs.starfarer.api.campaign.CargoPickerListener;
+import com.fs.starfarer.api.campaign.CargoStackAPI;
+import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.campaign.econ.CommoditySpecAPI;
+import com.fs.starfarer.api.impl.campaign.CoreReputationPlugin;
+import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.impl.campaign.ids.Strings;
+import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.AICores;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+import exerelin.campaign.DiplomacyManager;
+import exerelin.campaign.PlayerFactionStore;
+import exerelin.utilities.ExerelinFactionConfig;
+import exerelin.utilities.ExerelinUtilsFaction;
+import exerelin.utilities.NexUtilsReputation;
+import exerelin.utilities.StringHelper;
+
+public class Nex_AICores extends AICores {
+	
+	// Uses Nexerelin reputation function
+	// Prints relationship limit if applicable
+	// External strings
+	@Override
+	protected void selectCores() {
+		CargoAPI copy = Global.getFactory().createCargo(false);
+		//copy.addAll(cargo);
+		for (CargoStackAPI stack : playerCargo.getStacksCopy()) {
+			CommoditySpecAPI spec = stack.getResourceIfResource();
+			if (spec != null && spec.getDemandClass().equals(Commodities.AI_CORES)) {
+				copy.addFromStack(stack);
+			}
+		}
+		copy.sort();
+		
+		final float width = 310f;
+		dialog.showCargoPickerDialog(StringHelper.getString("exerelin_misc", "aiCoresSelect"), 
+				Misc.ucFirst(StringHelper.getString("confirm")), 
+				Misc.ucFirst(StringHelper.getString("cancel")),
+						true, width, copy, new CargoPickerListener() {
+			public void pickedCargo(CargoAPI cargo) {
+				cargo.sort();
+				for (CargoStackAPI stack : cargo.getStacksCopy()) {
+					playerCargo.removeItems(stack.getType(), stack.getData(), stack.getSize());
+					if (stack.isCommodityStack()) { // should be always, but just in case
+						AddRemoveCommodity.addCommodityLossText(stack.getCommodityId(), (int) stack.getSize(), text);
+					}
+				}
+				
+				float bounty = computeCoreCreditValue(cargo);
+				float repChange = computeCoreReputationValue(cargo);
+
+				if (bounty > 0) {
+					playerCargo.getCredits().add(bounty);
+					AddRemoveCommodity.addCreditsGainText((int)bounty, text);
+				}
+				
+				if (repChange >= 1f) {
+					CoreReputationPlugin.CustomRepImpact impact = new CoreReputationPlugin.CustomRepImpact();
+					impact.delta = repChange * 0.01f;
+					Global.getSector().adjustPlayerReputation(
+							new CoreReputationPlugin.RepActionEnvelope(CoreReputationPlugin.RepActions.CUSTOM, impact,
+												  null, text, true), 
+												  faction.getId());
+					
+					impact.delta *= 0.25f;
+					if (impact.delta >= 0.01f) {
+						Global.getSector().adjustPlayerReputation(
+								new CoreReputationPlugin.RepActionEnvelope(CoreReputationPlugin.RepActions.CUSTOM, impact,
+													  null, text, true), 
+													  person);
+					}
+				}
+				
+				FireBest.fire(null, dialog, memoryMap, "AICoresTurnedIn");
+			}
+			public void cancelledCargoSelection() {
+			}
+			public void recreateTextPanel(TooltipMakerAPI panel, CargoAPI cargo, CargoStackAPI pickedUp, boolean pickedUpFromSource, CargoAPI combined) {
+			
+				float bounty = computeCoreCreditValue(combined);
+				float repChange = computeCoreReputationValue(combined);
+				
+				float pad = 3f;
+				float small = 5f;
+				float opad = 10f;
+
+				panel.setParaOrbitronLarge();
+				panel.addPara(Misc.ucFirst(faction.getDisplayName()), faction.getBaseUIColor(), opad);
+				//panel.addPara(faction.getDisplayNameLong(), faction.getBaseUIColor(), opad);
+				//panel.addPara(faction.getDisplayName() + " (" + entity.getMarket().getName() + ")", faction.getBaseUIColor(), opad);
+				panel.setParaFontDefault();
+				
+				panel.addImage(faction.getLogo(), width * 1f, pad);
+				
+				//panel.setParaFontColor(Misc.getGrayColor());
+				//panel.setParaSmallInsignia();
+				//panel.setParaInsigniaLarge();
+				String str = StringHelper.getStringAndSubstituteToken("exerelin_misc",
+						"aiCoresMsg1", "$faction", faction.getDisplayNameLongWithArticle());
+				panel.addPara(str, opad);
+				panel.beginGridFlipped(width, 1, 40f, 10f);
+				//panel.beginGrid(150f, 1);
+				panel.addToGrid(0, 0, StringHelper.getString("exerelin_misc", "aiCoresBounty"),
+						"" + (int)(valueMult * 100f) + "%");
+				panel.addToGrid(0, 1, StringHelper.getString("exerelin_misc", "aiCoresRep"),
+						"" + (int)(repMult * 100f) + "%");
+				panel.addGrid(pad);
+				
+				str = StringHelper.getStringAndSubstituteToken("exerelin_misc",
+						"aiCoresMsg2", "$faction", faction.getDisplayNameWithArticle());
+				panel.addPara(str, 	opad * 1f, Misc.getHighlightColor(),
+						Misc.getWithDGS(bounty) + Strings.C, "" + (int) repChange);
+				
+				if (!DiplomacyManager.isRandomFactionRelationships())
+				{
+					FactionAPI myFaction = PlayerFactionStore.getPlayerFaction();
+					String shortName = ExerelinUtilsFaction.getFactionShortName(faction);
+					float maxRep = ExerelinFactionConfig.getMaxRelationship(faction.getId(), myFaction.getId());
+					if (maxRep < 1)
+					{
+						int maxRepInt = (int)(maxRep * 100f);
+						str = StringHelper.getStringAndSubstituteToken("exerelin_factions", 
+								"repLimit", "$faction", shortName);
+						panel.setParaFontColor(Misc.getGrayColor());
+						panel.addPara(str, opad * 1f, Misc.getHighlightColor(), 
+								maxRepInt + "/100", NexUtilsReputation.getRelationStr(myFaction, faction));
+						panel.setParaFontColor(Misc.getTextColor());
+					}
+				}				
+				
+				//panel.addPara("Bounty: %s", opad, Misc.getHighlightColor(), Misc.getWithDGS(bounty) + Strings.C);
+				//panel.addPara("Reputation: %s", pad, Misc.getHighlightColor(), "+12");
+			}
+		});
+	}
+	
+}
