89f9a5483c6facb2adb96857e2fc6bbd9411c28f
L.J. Lim <kingraptorowns@gmail.com>
2018-09-22 17:32:44 +0800

    Much more useful SpawnInvasionFleet and SpawnRespawnFleet console commands Remove SpawnInvasionFleetHostile

--- jars/sources/ExerelinCore/exerelin/console/commands/SpawnInvasionFleet.java
+++ jars/sources/ExerelinCore/exerelin/console/commands/SpawnInvasionFleet.java
@@ -3,81 +3,152 @@ package exerelin.console.commands;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.FactionAPI;
-import com.fs.starfarer.api.campaign.SectorAPI;
+import com.fs.starfarer.api.campaign.FleetAssignment;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.util.Misc;
-import exerelin.campaign.PlayerFactionStore;
+import com.fs.starfarer.api.util.WeightedRandomPicker;
+import exerelin.campaign.SectorManager;
 import exerelin.utilities.ExerelinUtilsFaction;
 import exerelin.campaign.fleets.InvasionFleetManager;
+import exerelin.utilities.ExerelinUtilsMarket;
+import java.util.ArrayList;
 import java.util.List;
 import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommandUtils;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
+import org.lazywizard.lazylib.CollectionUtils;
 import org.lwjgl.util.vector.Vector2f;
 
 public class SpawnInvasionFleet implements BaseCommand {
 
-    @Override
-    public CommandResult runCommand(String args, CommandContext context) {
-        if (context != CommandContext.CAMPAIGN_MAP) {
-            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
-            return CommandResult.WRONG_CONTEXT;
-        }
-        
-        SectorAPI sector = Global.getSector();
-        String playerAlignedFactionId = PlayerFactionStore.getPlayerFactionId();
-        FactionAPI playerAlignedFaction = sector.getFaction(playerAlignedFactionId);
-        CampaignFleetAPI playerFleet = sector.getPlayerFleet();
-        List<MarketAPI> targets = Global.getSector().getEconomy().getMarketsCopy();    //Misc.getMarketsInLocation(playerFleet.getContainingLocation());
-        List<MarketAPI> sources = ExerelinUtilsFaction.getFactionMarkets(playerAlignedFactionId);
-        
-        Vector2f playerPos = playerFleet.getLocationInHyperspace();
-        MarketAPI closestTargetMarket = null;
-        float closestTargetDist = 9999999;
-        MarketAPI closestOriginMarket = null;
-        float closestOriginDist = 9999999;
-        
-        // pick target market
-        for (MarketAPI market : targets) {
-            if (market.getFaction() == playerAlignedFaction) continue;
-            float distance = Misc.getDistance(playerPos, market.getPrimaryEntity().getLocationInHyperspace());            
-            if (distance < closestTargetDist)
-            {
-                closestTargetDist = distance;
-                closestTargetMarket = market;
-            }
-        }
-        
-        // pick source market
-        for (MarketAPI market : sources) {
-            float distance = Misc.getDistance(playerPos, market.getPrimaryEntity().getLocationInHyperspace());
-            if (distance < closestOriginDist)
-            {
-                closestOriginDist = distance;
-                closestOriginMarket = market;
-            }
-        }
-        
-        if (closestTargetMarket == null || closestOriginMarket == null)
-        {
-            Console.showMessage("Unable to find origin and/or target");
-            return CommandResult.ERROR;
-        }
-        
-        // spawn fleet
-        InvasionFleetManager.InvasionFleetData data = InvasionFleetManager.spawnInvasionFleet(playerAlignedFaction, closestOriginMarket, closestTargetMarket, 1.1f, true);
-        if (data == null) {
-            Console.showMessage("Unable to spawn fleet");
-            return CommandResult.ERROR;
-        }
-        if (closestOriginMarket.getContainingLocation() != playerFleet.getContainingLocation())
-        {
-            closestOriginMarket.getContainingLocation().removeEntity(data.fleet);
-            playerFleet.getContainingLocation().addEntity(data.fleet);
-        }
-        Console.showMessage("Spawning " + data.fleet.getName() + " from " + closestOriginMarket.getName());
-        Console.showMessage("Oscar Mike to " + closestTargetMarket.getName());
-        data.fleet.setLocation(playerFleet.getLocation().x, playerFleet.getLocation().y);
-        return CommandResult.SUCCESS;
-    }
+	@Override
+	public CommandResult runCommand(String args, CommandContext context) {
+		if (context != CommandContext.CAMPAIGN_MAP) {
+			Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
+			return CommandResult.WRONG_CONTEXT;
+		}
+		String[] tmp = args.split(" ");
+		
+		// get source faction
+		String factionId;
+		if (tmp.length == 0 || tmp[0].isEmpty() || tmp[0].equalsIgnoreCase("any"))
+		{
+			WeightedRandomPicker<String> picker = new WeightedRandomPicker<>();
+			List<String> factions = SectorManager.getLiveFactionIdsCopy();
+			picker.addAll(factions);
+			factionId = picker.pick();
+		}
+		else
+			factionId = tmp[0];
+		
+		FactionAPI faction = getFaction(factionId);
+		if (faction == null) {
+			return CommandResult.ERROR;
+		}
+		
+		// get target faction
+		String targetFactionId;
+		if (tmp.length <= 1 || tmp[1].isEmpty() || tmp[1].equalsIgnoreCase("any"))
+		{
+			// any hostile market is elgible
+			targetFactionId = null;
+		}
+		else
+			targetFactionId = tmp[1];
+		
+		FactionAPI targetFaction = getFaction(targetFactionId);
+		//if (targetFaction == null) return CommandResult.ERROR;
+		
+		// get source and target markets
+		CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
+		
+		List<MarketAPI> sources = ExerelinUtilsFaction.getFactionMarkets(faction.getId());
+		List<MarketAPI> targets;
+		if (targetFaction != null) targets = ExerelinUtilsFaction.getFactionMarkets(targetFaction.getId());
+		else targets = Global.getSector().getEconomy().getMarketsCopy();
+		
+		Vector2f playerPos = playerFleet.getLocationInHyperspace();
+		MarketAPI closestTargetMarket = null;
+		float closestTargetDist = 9999999;
+		MarketAPI closestOriginMarket = null;
+		float closestOriginDist = 9999999;
+		
+		// pick target market
+		for (MarketAPI market : targets) {
+			if (!ExerelinUtilsMarket.canBeInvaded(market, false))
+				continue;
+			if (!market.getFaction().isHostileTo(faction))
+				continue;
+			float distance = Misc.getDistance(playerPos, market.getPrimaryEntity().getLocationInHyperspace());			
+			if (distance < closestTargetDist)
+			{
+				closestTargetDist = distance;
+				closestTargetMarket = market;
+			}
+		}
+		
+		// pick source market
+		for (MarketAPI market : sources) {
+			float distance = Misc.getDistance(playerPos, market.getPrimaryEntity().getLocationInHyperspace());
+			if (distance < closestOriginDist)
+			{
+				closestOriginDist = distance;
+				closestOriginMarket = market;
+			}
+		}
+		
+		if (closestTargetMarket == null || closestOriginMarket == null)
+		{
+			Console.showMessage("Unable to find origin and/or target");
+			return CommandResult.ERROR;
+		}
+		
+		// spawn fleet
+		InvasionFleetManager.InvasionFleetData data = InvasionFleetManager.spawnInvasionFleet(
+				faction, closestOriginMarket, closestTargetMarket, 1.1f, true);
+		if (data == null) {
+			Console.showMessage("Unable to spawn fleet");
+			return CommandResult.ERROR;
+		}
+		if (closestOriginMarket.getContainingLocation() != playerFleet.getContainingLocation())
+		{
+			closestOriginMarket.getContainingLocation().removeEntity(data.fleet);
+			playerFleet.getContainingLocation().addEntity(data.fleet);
+		}
+		Console.showMessage("Spawning " + data.fleet.getNameWithFaction() + " from " + closestOriginMarket.getName());
+		Console.showMessage("Oscar Mike to " + closestTargetMarket.getName() + " (" + closestTargetMarket.getFaction().getDisplayName()
+				+ ") in " + closestTargetMarket.getContainingLocation().getName());
+		data.fleet.setLocation(playerFleet.getLocation().x, playerFleet.getLocation().y);
+		data.fleet.addAssignmentAtStart(FleetAssignment.STANDING_DOWN, data.fleet, 0.5f, null);	// so it doesn't instantly attack player
+		return CommandResult.SUCCESS;
+	}
+	
+	/**
+	 * Gets best faction match; prints error message if not found.
+	 * @param factionId
+	 * @return 
+	 */
+	public static FactionAPI getFaction(String factionId)
+	{
+		if (factionId == null)
+		{
+			return null;
+		}
+		
+		FactionAPI faction = CommandUtils.findBestFactionMatch(factionId);
+		if (faction == null)
+		{
+			final List<String> ids = new ArrayList<>();
+			for (FactionAPI existsFaction : Global.getSector().getAllFactions())
+			{
+				ids.add(existsFaction.getId());
+			}
+			
+			Console.showMessage("Error: no such faction '" + factionId
+					+ "'! Valid factions: " + CollectionUtils.implode(ids) + ".");
+			return null;
+		}
+		return faction;
+	}
 }
--- jars/sources/ExerelinCore/exerelin/console/commands/SpawnRespawnFleet.java
+++ jars/sources/ExerelinCore/exerelin/console/commands/SpawnRespawnFleet.java
@@ -3,81 +3,66 @@ package exerelin.console.commands;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.FactionAPI;
-import com.fs.starfarer.api.campaign.SectorAPI;
-import com.fs.starfarer.api.campaign.econ.MarketAPI;
-import com.fs.starfarer.api.util.Misc;
+import com.fs.starfarer.api.campaign.FleetAssignment;
 import com.fs.starfarer.api.util.WeightedRandomPicker;
-import exerelin.ExerelinConstants;
-import exerelin.campaign.PlayerFactionStore;
 import exerelin.campaign.SectorManager;
-import exerelin.campaign.fleets.InvasionFleetManager;
+import exerelin.campaign.fleets.InvasionFleetManager.InvasionFleetData;
 import exerelin.utilities.ExerelinConfig;
 import java.util.List;
 import org.lazywizard.console.BaseCommand;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
-import org.lwjgl.util.vector.Vector2f;
 
 public class SpawnRespawnFleet implements BaseCommand {
 
-    @Override
-    public CommandResult runCommand(String args, CommandContext context) {
-        if (context != CommandContext.CAMPAIGN_MAP) {
-            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
-            return CommandResult.WRONG_CONTEXT;
-        }
-        
-        SectorAPI sector = Global.getSector();
-        String playerAlignedFactionId = PlayerFactionStore.getPlayerFactionId();
-        FactionAPI playerAlignedFaction = sector.getFaction(playerAlignedFactionId);
-        CampaignFleetAPI playerFleet = sector.getPlayerFleet();
-        List<MarketAPI> markets = Global.getSector().getEconomy().getMarketsCopy();
-        
-        Vector2f playerPos = playerFleet.getLocationInHyperspace();
-        MarketAPI closestTargetMarket = null;
-        float closestTargetDist = 9999999;
-        
-        // pick faction
-        List<String> factions = ExerelinConfig.getFactions(true, false);
-        WeightedRandomPicker<String> factionPicker = new WeightedRandomPicker<>();
-        for (String faction : factions)
-        {
-            if (SectorManager.isFactionAlive(faction)) continue;
-            if (faction.equals(ExerelinConstants.PLAYER_NPC_ID)) continue;
-            factionPicker.add(faction);
-        }
-        String factionId = factionPicker.pick();
-        if (factionId == null) factionId = playerAlignedFactionId;
-        
-        // pick target market
-        for (MarketAPI market : markets) {
-            //if (market.getFaction() == playerAlignedFaction) continue;
-            if (market.getContainingLocation() != playerFleet.getContainingLocation()) continue;
-            float distance = Misc.getDistance(playerPos, market.getPrimaryEntity().getLocation());
-            if (distance < closestTargetDist)
-            {
-                closestTargetDist = distance;
-                closestTargetMarket = market;
-            }
-        }
-        
-        if (closestTargetMarket == null)
-        {
-            Console.showMessage("Unable to find target");
-                return CommandResult.ERROR;
-        }
-        
-        // spawn fleet
-        InvasionFleetManager.InvasionFleetData data = InvasionFleetManager.spawnRespawnFleet(Global.getSector().getFaction(factionId), 
-                closestTargetMarket, closestTargetMarket, false);
-        if (data == null) {
-            Console.showMessage("Unable to spawn fleet");
-            return CommandResult.ERROR;
-        }
-        Console.showMessage("Spawning " + data.fleet.getName() + ", sending to " + closestTargetMarket.getName());
-        data.fleet.getContainingLocation().removeEntity(data.fleet);
-        playerFleet.getContainingLocation().addEntity(data.fleet);
-        data.fleet.setLocation(playerFleet.getLocation().x, playerFleet.getLocation().y);
-        return CommandResult.SUCCESS;
-    }
+	@Override
+	public CommandResult runCommand(String args, CommandContext context) {
+		if (context != CommandContext.CAMPAIGN_MAP) {
+			Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
+			return CommandResult.WRONG_CONTEXT;
+		}
+		String[] tmp = args.split(" ");
+		
+		// get faction to respawn
+		String factionId;
+		if (tmp.length == 0 || tmp[0].isEmpty())
+		{
+			WeightedRandomPicker<String> picker = new WeightedRandomPicker<>();
+			List<String> factions = ExerelinConfig.getFactions(true, false);
+			for (String faction : factions)
+			{
+				if (SectorManager.isFactionAlive(faction)) continue;
+				picker.add(faction);
+			}
+			factionId = picker.pick();
+		}
+		else
+			factionId = tmp[0];
+		
+		if (factionId == null)
+		{
+			Console.showMessage("Error: No available faction to respawn");
+			return CommandResult.ERROR;
+		}
+		
+		FactionAPI faction = SpawnInvasionFleet.getFaction(factionId);
+		if (faction == null) return CommandResult.ERROR;
+			
+		// spawn fleet
+		InvasionFleetData data = SectorManager.spawnRespawnFleet(faction, null, true);
+		if (data == null) {
+			Console.showMessage("Unable to spawn fleet");
+			return CommandResult.ERROR;
+		}
+		
+		CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
+		Console.showMessage("Spawning " + data.fleet.getNameWithFaction() + " from " + data.sourceMarket.getName());
+		Console.showMessage("Oscar Mike to " + data.targetMarket.getName() + " (" + data.target.getFaction().getDisplayName() 
+				+ ") in " + data.target.getContainingLocation().getName());
+		data.fleet.getContainingLocation().removeEntity(data.fleet);
+		playerFleet.getContainingLocation().addEntity(data.fleet);
+		data.fleet.setLocation(playerFleet.getLocation().x, playerFleet.getLocation().y);
+		data.fleet.addAssignmentAtStart(FleetAssignment.STANDING_DOWN, data.fleet, 0.5f, null);	// so it doesn't instantly attack player
+		return CommandResult.SUCCESS;
+	}
 }
