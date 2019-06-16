Histidine91/Nexerelin

commit b07512ada59754cfd8c86ddac8021873cf553d53

Fix offensive fleet intel display Only name "original owner" as holding faction if the target currently belongs to own faction

jars/sources/ExerelinCore/exerelin/campaign/intel/OffensiveFleetIntel.java
@@ -190,7 +190,8 @@ public abstract class OffensiveFleetIntel extends RaidIntel implements RaidDeleg
 		
 		bullet(info);
 		
-		FactionAPI other = targetFaction;
+		FactionAPI other = target.getFaction();
+		if (other == faction) other = targetFaction;
 		
 		info.addPara(StringHelper.getString("faction", true) + ": " + faction.getDisplayName(), initPad, tc,
 				 	 faction.getBaseUIColor(), faction.getDisplayName());

jars/sources/ExerelinCore/exerelin/campaign/intel/invasion/InvasionIntel.java
@@ -128,7 +128,8 @@ public class InvasionIntel extends OffensiveFleetIntel implements RaidDelegate {
 		info.addImage(getFactionForUIColors().getLogo(), width, 128, opad);
 		
 		FactionAPI attacker = getFaction();
-		FactionAPI defender = targetFaction;
+		FactionAPI defender = target.getFaction();
+		if (defender == attacker) defender = targetFaction;
 		String has = attacker.getDisplayNameHasOrHave();
 		String is = attacker.getDisplayNameIsOrAre();
 		String locationName = target.getContainingLocation().getNameWithLowercaseType();

commit 1143227ad4f620c5bf74a502a172340ba7cdf902

Fix colony expedition crash introduced in b07512ada59754cfd8c86ddac8021873cf553d53

jars/sources/ExerelinCore/exerelin/campaign/intel/OffensiveFleetIntel.java
@@ -190,7 +190,8 @@ public abstract class OffensiveFleetIntel extends RaidIntel implements RaidDeleg
 		
 		bullet(info);
 		
-		FactionAPI other = target.getFaction();
+		FactionAPI other = targetFaction;
+		if (target != null) other = target.getFaction();	// target is null for colony expeditions
 		if (other == faction) other = targetFaction;
 		
 		info.addPara(StringHelper.getString("faction", true) + ": " + faction.getDisplayName(), initPad, tc,