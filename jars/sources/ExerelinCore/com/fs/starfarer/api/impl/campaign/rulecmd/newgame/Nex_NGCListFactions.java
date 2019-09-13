282b17a44009d9fdb7fdb3b440e485fe46931cfa
L.J. Lim <kingraptorowns@gmail.com>
2019-09-13 21:28:24 +0800

    Paginated starting factions list, easy version

--- jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/newgame/Nex_NGCListFactions.java
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/newgame/Nex_NGCListFactions.java
@@ -9,6 +9,7 @@ import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.impl.campaign.rulecmd.Nex_FactionDirectoryHelper;
 import com.fs.starfarer.api.impl.campaign.rulecmd.Nex_FactionDirectoryHelper.FactionListGrouping;
+import com.fs.starfarer.api.impl.campaign.rulecmd.PaginatedOptions;
 import com.fs.starfarer.api.util.Misc;
 import exerelin.campaign.ExerelinSetupData;
 import exerelin.utilities.ExerelinConfig;
@@ -16,13 +17,15 @@ import exerelin.utilities.ExerelinFactionConfig;
 import exerelin.utilities.ExerelinUtils;
 import exerelin.utilities.StringHelper;
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.lwjgl.input.Keyboard;
 
-public class Nex_NGCListFactions extends BaseCommandPlugin {
+public class Nex_NGCListFactions extends PaginatedOptions {
 	
 	public static final String JOIN_FACTION_OPTION_PREFIX = "nex_NGCJoinFaction_";
+	protected static final List<Misc.Token> EMPTY_PARAMS = new ArrayList<>();
 	
 	@Override
 	public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
@@ -31,6 +34,8 @@ public class Nex_NGCListFactions extends BaseCommandPlugin {
 		switch (arg)
 		{
 			case "listGroups":
+				optionsPerPage = 6;
+				super.execute(ruleId, dialog, EMPTY_PARAMS, memoryMap);
 				listGroups(dialog);
 				return true;
 				
@@ -40,6 +45,7 @@ public class Nex_NGCListFactions extends BaseCommandPlugin {
 				int num = (int)params.get(1).getFloat(memoryMap);
 				if (num == 0)
 				{
+					super.execute(ruleId, dialog, EMPTY_PARAMS, memoryMap);
 					listGroups(dialog);
 					return true;
 				}
@@ -90,33 +96,46 @@ public class Nex_NGCListFactions extends BaseCommandPlugin {
 		{
 			groupNum++;
 			String optionId = "nex_NGCListFactions" + groupNum;
-			opts.addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "factions")) + ": " + group.getGroupingRangeString(),
-					optionId, group.tooltip);
+			addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "factions")) + ": " + group.getGroupingRangeString(),
+					optionId);
+		}
+		addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "ownFaction")), "nex_NGCJoinOwnFaction");
+		addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "freeStartHard")), "nex_NGCFreeStart");
+		opts.setTooltip("nex_NGCFreeStart", StringHelper.getString("exerelin_ngc", "freeStartTooltip"));
+		
+		addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "randomFaction")), "nex_NGCJoinRandomFaction");
+		addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "customStart")), "nex_NGCCustomStart");
+
+		if (Global.getSettings().isDevMode())
+		{
+			addOption(StringHelper.getString("exerelin_ngc", "devStart"), "nex_NGCDevStart");
+			addOption(StringHelper.getString("exerelin_ngc", "devStartFast"), "nex_NGCDevStartFast");
+			//ExerelinUtils.addDevModeDialogOptions(dialog);
+		}
+		addOptionAllPages(Misc.ucFirst(StringHelper.getString("back")), "exerelinNGCOptionsBack");
+				
+		showAndProcessGroupOptions(dialog.getOptionPanel(), groups);
+	}
+	
+	public void showAndProcessGroupOptions(OptionPanelAPI opts, List<FactionListGrouping> groups) 
+	{
+		super.showOptions();
+		int groupNum = 0;
+		for (FactionListGrouping group : groups)
+		{
+			groupNum++;
+			String optionId = "nex_NGCListFactions" + groupNum;
+			opts.setTooltip(optionId, group.tooltip);
 			opts.setTooltipHighlights(optionId, group.getFactionNames().toArray(new String[0]));
 			opts.setTooltipHighlightColors(optionId, group.getTooltipColors().toArray(new Color[0]));
 		}
-		opts.addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "ownFaction")), "nex_NGCJoinOwnFaction");
+		
 		if (ExerelinSetupData.getInstance().corvusMode)
 		{
 			opts.setTooltip("nex_NGCJoinOwnFaction", StringHelper.getString("exerelin_ngc", "ownFactionDisabledTooltip"));
 			opts.setEnabled("nex_NGCJoinOwnFaction", false);
 		}
-		opts.addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "freeStartHard")), "nex_NGCFreeStart");
-		opts.setTooltip("nex_NGCFreeStart", StringHelper.getString("exerelin_ngc", "freeStartTooltip"));
 		
-		opts.addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "randomFaction")), "nex_NGCJoinRandomFaction");
-		
-		opts.addOption(Misc.ucFirst(StringHelper.getString("exerelin_ngc", "customStart")), "nex_NGCCustomStart");
-
-		if (Global.getSettings().isDevMode())
-		{
-			opts.addOption(StringHelper.getString("exerelin_ngc", "devStart"), "nex_NGCDevStart");
-			opts.addOption(StringHelper.getString("exerelin_ngc", "devStartFast"), "nex_NGCDevStartFast");
-			ExerelinUtils.addDevModeDialogOptions(dialog);
-		}
-
-		opts.addOption(Misc.ucFirst(StringHelper.getString("back")), "exerelinNGCOptionsBack");
 		opts.setShortcut("exerelinNGCOptionsBack", Keyboard.KEY_ESCAPE, false, false, false, false);
-
 	}
 }
