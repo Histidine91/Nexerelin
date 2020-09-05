6e8c007d07d70484a92944ec01fdaa1332d67797
L.J. Lim <kingraptorowns@gmail.com>
2020-09-05 14:26:32 +0800

    Take over commission intel  Fix a case where player becoming non-hostile to a faction would be reverted by still-hostile commissioning faction  Fix intel item not disappearing when ended

--- jars/sources/ExerelinCore/exerelin/campaign/intel/Nex_FactionCommissionIntel.java
+++ jars/sources/ExerelinCore/exerelin/campaign/intel/Nex_FactionCommissionIntel.java
@@ -1,8 +1,11 @@
 package exerelin.campaign.intel;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.impl.campaign.ids.MemFlags;
 import com.fs.starfarer.api.impl.campaign.intel.FactionCommissionIntel;
+import static com.fs.starfarer.api.impl.campaign.intel.FactionCommissionIntel.log;
 import com.fs.starfarer.api.util.Misc;
 
 public class Nex_FactionCommissionIntel extends FactionCommissionIntel {
@@ -19,5 +22,20 @@ public class Nex_FactionCommissionIntel extends FactionCommissionIntel {
 	@Override
 	public String getName() {
 		return Misc.ucFirst(super.getName());
+	}
+	
+	// Do not remove the script when mission ends, that keeps it from expiring
+	@Override
+	public void endMission(InteractionDialogAPI dialog) {
+		log.info(String.format("Ending commission with [%s]", faction.getDisplayName()));
+		Global.getSector().getListenerManager().removeListener(this);
+		//Global.getSector().removeScript(this);
+		
+		Global.getSector().getCharacterData().getMemoryWithoutUpdate().unset(MemFlags.FCM_FACTION);
+		Global.getSector().getCharacterData().getMemoryWithoutUpdate().unset(MemFlags.FCM_EVENT);
+		
+		undoAllRepChanges(dialog);
+		
+		endAfterDelay();
 	}
 }
