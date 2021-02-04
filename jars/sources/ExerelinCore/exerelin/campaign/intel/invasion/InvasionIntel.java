034b9b99f6bd54960aa4f0b76f8a7b1b7f9e8d9d
nikothedude <simon.prouty@gmail.com>
2021-02-04 00:25:06 -0500

    makes invasion intel (and only invasion intel) send the player a notification if they have intel and the invasion ends

--- jars/sources/ExerelinCore/exerelin/campaign/intel/invasion/InvasionIntel.java
+++ jars/sources/ExerelinCore/exerelin/campaign/intel/invasion/InvasionIntel.java
@@ -537,6 +537,7 @@ public class InvasionIntel extends OffensiveFleetIntel implements RaidDelegate {
 	@Override
 	protected void notifyEnding() {
 		log.info("Invasion event ending");
+		sendOutcomeUpdate();
 		super.notifyEnding();
 		if (brawlDefIntel != null && brawlDefIntel.getOutcome() == null) {
 			log.info("Setting outcome for brawl defense, " + outcome.toString());
