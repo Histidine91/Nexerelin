94c15eb64fb2929641daf1faca9749667dbe5b55
L.J. Lim <kingraptorowns@gmail.com>
2021-03-11 22:55:04 +0800

    When docking at a market, print the original owner to dialog text if it's not the current owner

---
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/Nex_PrintOriginalOwner.java
@@ -0,0 +1,30 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.TextPanelAPI;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.util.Misc;
+import exerelin.utilities.ExerelinUtilsMarket;
+import exerelin.utilities.StringHelper;
+import java.util.List;
+import java.util.Map;
+
+public class Nex_PrintOriginalOwner extends BaseCommandPlugin {
+    
+    @Override
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        MarketAPI market = dialog.getInteractionTarget().getMarket();
+		String origOwnerId = ExerelinUtilsMarket.getOriginalOwner(market);
+		FactionAPI origOwner = Global.getSector().getFaction(origOwnerId);
+		
+		TextPanelAPI text = dialog.getTextPanel();
+		text.setFontSmallInsignia();
+		text.addPara(StringHelper.getString("exerelin_markets", "originalOwner"), origOwner.getBaseUIColor(), origOwner.getDisplayName());
+		text.setFontInsignia();
+		
+        return true;
+    }
+}
