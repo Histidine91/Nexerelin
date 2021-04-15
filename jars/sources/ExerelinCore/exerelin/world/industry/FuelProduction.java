8e17ca6d460645f132d71bde30341c69c96d7c97
L.J. Lim <kingraptorowns@gmail.com>
2021-04-15 11:19:54 +0800

    Fix/improve fuel production seeding in random sector * Make sure synchrotron gets placed where it actually functions, and try to place fuel production accordingly * Hegemony gets a synchrotron, matching non-random sector

--- jars/sources/ExerelinCore/exerelin/world/industry/FuelProduction.java
+++ jars/sources/ExerelinCore/exerelin/world/industry/FuelProduction.java
@@ -5,25 +5,42 @@ import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.econ.MarketConditionAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
 import com.fs.starfarer.api.impl.campaign.ids.Conditions;
+import com.fs.starfarer.api.impl.campaign.ids.Factions;
 import com.fs.starfarer.api.impl.campaign.ids.Industries;
 import com.fs.starfarer.api.impl.campaign.procgen.StarSystemGenerator;
 import exerelin.campaign.econ.EconomyInfoHelper;
+import exerelin.utilities.NexConfig;
+import exerelin.utilities.NexFactionConfig.BonusSeed;
 import exerelin.world.ExerelinProcGen;
 import exerelin.world.ExerelinProcGen.ProcGenEntity;
-import exerelin.world.NexMarketBuilder;
+import exerelin.world.industry.bonus.Synchrotron;
+import java.util.List;
 
 public class FuelProduction extends IndustryClassGen {
 
 	public FuelProduction() {
 		super(Industries.FUELPROD);
 	}
+	
+	public static boolean factionHasSynchrotronSeed(String factionId) {
+		List<BonusSeed> seeds = NexConfig.getFactionConfig(factionId).bonusSeeds;
+		for (BonusSeed seed : seeds) {
+			if (seed.id.equals("synchrotron")) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	@Override
 	public float getWeight(ProcGenEntity entity) {
 		boolean newGame = Global.getSector().isInNewGameAdvance();
+		boolean factionHasSynchrotron = factionHasSynchrotronSeed(entity.market.getFactionId());
 		
 		// hax so this isn't the default choice for little stations
-		if (newGame && entity.type == ExerelinProcGen.EntityType.STATION && StarSystemGenerator.random.nextBoolean())
+		if (newGame && entity.type == ExerelinProcGen.EntityType.STATION 
+				&& !factionHasSynchrotron
+				&& StarSystemGenerator.random.nextBoolean())
 			return -1;
 		
 		MarketAPI market = entity.market;
@@ -53,10 +70,14 @@ public class FuelProduction extends IndustryClassGen {
 		weight += (150 - market.getHazardValue()) * 2;
 		
 		// prefer airless worlds to support our synchrotron
-		if (market.hasCondition(Conditions.NO_ATMOSPHERE))
-			weight *= 2;
-		else
-			weight /= 2;
+		if (factionHasSynchrotron && Synchrotron.isSynchrotronApplicable(market)) {
+			if (newGame) weight *= 9999;
+			else weight *= 2;
+		}
+		else {
+			if (newGame) weight /= 5;
+			else weight /= 2;
+		}
 		
 		// prefer to not be on same planet as heavy industry
 		if (HeavyIndustry.hasHeavyIndustry(market))
--- jars/sources/ExerelinCore/exerelin/world/industry/bonus/Synchrotron.java
+++ jars/sources/ExerelinCore/exerelin/world/industry/bonus/Synchrotron.java
@@ -2,6 +2,8 @@ package exerelin.world.industry.bonus;
 
 import com.fs.starfarer.api.campaign.SpecialItemData;
 import com.fs.starfarer.api.campaign.econ.Industry;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Conditions;
 import com.fs.starfarer.api.impl.campaign.ids.Industries;
 import com.fs.starfarer.api.impl.campaign.ids.Items;
 import exerelin.world.ExerelinProcGen;
@@ -12,10 +14,17 @@ public class Synchrotron extends BonusGen {
 		super(Industries.FUELPROD);
 	}
 	
+	public static boolean isSynchrotronApplicable(MarketAPI market) {
+		return market.hasCondition(Conditions.NO_ATMOSPHERE) || market.getPlanetEntity() == null;
+	}
+	
 	@Override
 	public boolean canApply(Industry ind, ExerelinProcGen.ProcGenEntity entity) {
 		if (ind.getSpecialItem() != null)
 			return false;
+		if (!isSynchrotronApplicable(entity.market))
+			return false;
+		
 		return super.canApply(ind, entity);
 	}
 	
