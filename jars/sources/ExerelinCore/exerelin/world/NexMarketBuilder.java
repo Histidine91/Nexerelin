16e08b1003bfdb0b7ed0831e4dca601ec4f25843
L.J. Lim <kingraptorowns@gmail.com>
2021-04-17 11:04:36 +0800

    More SRA fixes

--- jars/sources/ExerelinCore/exerelin/world/NexMarketBuilder.java
+++ jars/sources/ExerelinCore/exerelin/world/NexMarketBuilder.java
@@ -655,8 +655,8 @@ public class NexMarketBuilder
 				//market.addIndustry("ii_interstellarbazaar");	// put somewhere else
 				market.addIndustry("ii_imperialguard");
 			}
-			else if (factionId.equals("shadow_industry"))
-				market.addIndustry("ms_redwingsCommand");
+			//else if (factionId.equals("shadow_industry"))	// now done in industry seeds
+			//	market.addIndustry("ms_redwingsCommand");
 			else if (factionId.equals("blackrock_driveyards"))
 				market.addIndustry("brdy_defhq");
 			else if (factionId.equals("scalartech"))
@@ -861,7 +861,10 @@ public class NexMarketBuilder
 					market.removeIndustry(Industries.HEAVYINDUSTRY, null, false);
 					highest.one.numProductiveIndustries -= 1;
 				}
-				else if (industryId.equals("ms_massIndustry")) {
+				else if (industryId.equals("ms_massIndustry") 
+						|| industryId.equals("ms_militaryProduction")
+						|| industryId.equals("ms_orbitalShipyard")) 
+				{
 					market.removeIndustry("ms_modularFac", null, false);
 					highest.one.numProductiveIndustries -= 1;
 				}
