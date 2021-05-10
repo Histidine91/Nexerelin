31a516484a86e2ca4f4658e7d513b01ee5f3618c
L.J. Lim <kingraptorowns@gmail.com>
2021-05-10 18:50:11 +0800

    Fix double bombardment bug

--- jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/salvage/Nex_MarketCMD.java
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/salvage/Nex_MarketCMD.java
@@ -1535,7 +1535,6 @@ public class Nex_MarketCMD extends MarketCMD {
 	// saturation bombardment affects disposition
 	@Override
 	protected void bombardConfirm() {
-		super.bombardConfirm();
 		if (temp.bombardType == null) {
 			bombardNeverMind();
 			return;
