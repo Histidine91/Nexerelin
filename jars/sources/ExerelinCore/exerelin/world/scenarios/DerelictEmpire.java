e49393014aa69e1cb8a402a08886aa81eefc5675
L.J. Lim <kingraptorowns@gmail.com>
2021-12-28 18:57:10 +0800

    Derelict Empire: Factions start with invasion meter 80% charged

--- jars/sources/ExerelinCore/exerelin/world/scenarios/DerelictEmpire.java
+++ jars/sources/ExerelinCore/exerelin/world/scenarios/DerelictEmpire.java
@@ -7,6 +7,8 @@ import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Factions;
 import exerelin.campaign.DiplomacyManager;
 import exerelin.campaign.SectorManager;
+import exerelin.campaign.fleets.InvasionFleetManager;
+import exerelin.utilities.NexConfig;
 import exerelin.utilities.NexUtilsFaction;
 import exerelin.world.ExerelinCorvusLocations;
 import java.util.HashSet;
@@ -47,6 +49,18 @@ public class DerelictEmpire extends Scenario {
 			if (factionId.equals("nex_derelict")) continue;
 			if (factionId.equals(Factions.REMNANTS)) continue;
 			derelict.setRelationship(factionId, DiplomacyManager.STARTING_RELATIONSHIP_HOSTILE);
+		}
+	}
+	
+	@Override
+	public void afterTimePass(SectorAPI sector) {
+		InvasionFleetManager man = InvasionFleetManager.getManager();
+		if (man == null) return;
+		
+		float points = NexConfig.pointsRequiredForInvasionFleet * 0.8f;
+		
+		for (String factionId : SectorManager.getLiveFactionIdsCopy()) {
+			man.modifySpawnCounter(factionId, points);
 		}
 	}
 }
