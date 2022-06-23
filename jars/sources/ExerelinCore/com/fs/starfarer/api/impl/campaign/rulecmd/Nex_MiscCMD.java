fe4b51e7d802dc1792d287a9515b3c1af203aaff
L.J. Lim <kingraptorowns@gmail.com>
2022-06-23 21:33:16 +0800

    Midnight interaction with SotF's Sierra

---
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/Nex_MiscCMD.java
@@ -0,0 +1,41 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.rules.MemKeys;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.util.Misc;
+import exerelin.utilities.NexUtilsReputation;
+import java.util.List;
+import java.util.Map;
+
+public class Nex_MiscCMD extends BaseCommandPlugin {
+	
+	@Override
+	public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) 
+	{
+		String arg = params.get(0).getString(memoryMap);
+		
+		switch(arg)
+		{
+			case "hasSierra":
+				return hasSierra(memoryMap.get(MemKeys.LOCAL));
+			default:
+				return false;
+		}
+	}
+	
+	public boolean hasSierra(MemoryAPI mem) {
+		if (Global.getSector().getPlayerFleet() != null) {
+			for (FleetMemberAPI member : Global.getSector().getPlayerFleet().getMembersWithFightersCopy()) {
+				if (member.getVariant().hasHullMod("fronsec_sierrasconcord")) {
+					//Global.getLogger(Nex_MiscCMD.class).info("Has sierra");
+					mem.set("$nex_relColor", NexUtilsReputation.getRelColor(-0.2f), 0);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
