7914cc18e245d4ac46bf2f7de2d2e933de265723
L.J. Lim <kingraptorowns@gmail.com>
2023-05-06 21:23:38 +0800

    Custom autoresolve plugin extends fixed vanilla plugin

---
+++ jars/sources/ExerelinCore/exerelin/campaign/battle/NexBattleAutoresolverPlugin.kt
@@ -0,0 +1,25 @@
+package exerelin.campaign.battle
+
+import com.fs.starfarer.api.campaign.BattleAPI
+import com.fs.starfarer.api.impl.campaign.BattleAutoresolverPluginImpl
+import com.fs.starfarer.api.fleet.FleetMemberAPI
+import com.fs.starfarer.api.impl.campaign.BattleAutoresolverPluginImpl.FleetMemberAutoresolveData
+import com.fs.starfarer.api.campaign.CampaignFleetAPI
+
+open class NexBattleAutoresolverPlugin(battle: BattleAPI?) : BattleAutoresolverPluginImpl(battle) {
+
+    companion object {
+        @JvmField val MEM_KEY_STRENGTH_MULT = "\$nex_autoresolve_strMult";
+    }
+
+    override fun computeDataForMember(member: FleetMemberAPI): FleetMemberAutoresolveData {
+        val data = super.computeDataForMember(member)
+
+        // MODIFIED
+        var fleetMult = member?.fleetData?.fleet?.memoryWithoutUpdate?.getFloat(MEM_KEY_STRENGTH_MULT) ?: 1f
+
+        // MODIFIED
+        data.strength *= fleetMult
+        return data
+    }
+}
