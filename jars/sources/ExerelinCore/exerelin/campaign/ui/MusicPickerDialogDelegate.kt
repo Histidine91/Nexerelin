26eaa9421ade8e96abc2fe4a9a87065fdf3bf5f6
L.J. Lim <kingraptorowns@gmail.com>
2023-07-22 14:45:11 +0800

    Music tracks not specified in a faction file can be added to music picker in data/config/modSettings.json

--- jars/sources/ExerelinCore/exerelin/campaign/ui/MusicPickerDialogDelegate.kt
+++ jars/sources/ExerelinCore/exerelin/campaign/ui/MusicPickerDialogDelegate.kt
@@ -7,11 +7,27 @@ import com.fs.starfarer.api.combat.EngagementResultAPI
 import com.fs.starfarer.api.ui.CustomPanelAPI
 import com.fs.starfarer.api.ui.IntelUIAPI
 import com.fs.starfarer.api.util.Misc
+import exerelin.ExerelinConstants
 import exerelin.campaign.intel.PersonalConfigIntel
+import exerelin.utilities.NexConfig
+import org.magiclib.util.MagicSettings
 import java.awt.Color
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.LinkedHashSet
 
 class MusicPickerDialogDelegate(val musicType: String, val width : Float, val height : Float, val intelUIAPI: IntelUIAPI, val intel: PersonalConfigIntel?) :
     BaseCustomDialogDelegate() {
+
+    companion object {
+        @JvmField val CUSTOM_TRACKS : MutableList<String> = MagicSettings.getList(ExerelinConstants.MOD_ID, "customFactionTracks")
+        @JvmField val CUSTOM_TRACK_COLORS = MagicSettings.getColorMap(ExerelinConstants.MOD_ID, "customFactionTrackColors")
+        @JvmField val CUSTOM_TRACK_FACTION_COLORS = MagicSettings.getStringMap(ExerelinConstants.MOD_ID, "customFactionTrackFactionColors")
+
+        init {
+            CUSTOM_TRACKS.sort();
+        }
+    }
 
     val customPanelPlugin = FramedCustomPanelPlugin(0.25f, Misc.getBasePlayerColor(), false)
     var mainPanel : CustomPanelAPI? = null
@@ -22,17 +38,38 @@ class MusicPickerDialogDelegate(val musicType: String, val width : Float, val he
         return customPanelPlugin
     }
 
+
+
     fun getMusicSetsAndColors(): Pair<Set<String>, Map<String, Color>> {
         val tracks = LinkedHashSet<String>()
         val colors = HashMap<String, Color>()
         for (faction in Global.getSector().allFactions) {
-            if (faction.isPlayerFaction || !faction.isShowInIntelTab) continue
-            tracks.addAll(faction.musicMap.values)
+            //if (!faction.isShowInIntelTab && !faction.isPlayerFaction) continue
+            if (!NexConfig.getFactionConfig(faction.id).playableFaction) continue
+            val sortedTracks = ArrayList<String>(faction.musicMap.values)
+            sortedTracks.sort()
+            tracks.addAll(sortedTracks)
             for (musicSet : String in faction.musicMap.values) {
                 if (colors.containsKey(musicSet)) continue
                 colors[musicSet] = faction.baseUIColor
             }
         }
+        for (track in CUSTOM_TRACKS) {
+            tracks.add(track)
+            colors[track] = Misc.getTextColor()
+        }
+        for (keypair in CUSTOM_TRACK_FACTION_COLORS) {
+            val track = keypair.key
+            val factionId = keypair.value
+            colors[track] = Global.getSector().getFaction(factionId).baseUIColor
+        }
+
+        for (keypair in CUSTOM_TRACK_COLORS) {
+            val track = keypair.key
+            val color = keypair.value
+            colors[track] = color
+        }
+
         return Pair(tracks, colors)
     }
 
