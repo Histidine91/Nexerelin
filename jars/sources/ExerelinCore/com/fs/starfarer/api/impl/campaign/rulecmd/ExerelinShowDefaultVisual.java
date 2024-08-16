dd5164b315a55c5af3faddf33420d66125c45860
L.J. Lim <kingraptorowns@gmail.com>
2024-08-16 21:07:31 +0800

    Fix Illustrated.Entities incompatibility on captured markets

--- jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/ExerelinShowDefaultVisual.java
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/ExerelinShowDefaultVisual.java
@@ -2,18 +2,13 @@ package com.fs.starfarer.api.impl.campaign.rulecmd;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.InteractionDialogImageVisual;
-import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
-import com.fs.starfarer.api.campaign.PlanetAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
-import com.fs.starfarer.api.campaign.rules.MemKeys;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.util.Misc.Token;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import exerelin.utilities.NexUtils;
+
+import java.util.*;
 
 public class ExerelinShowDefaultVisual extends ShowDefaultVisual {
 	
@@ -35,31 +30,21 @@ public class ExerelinShowDefaultVisual extends ShowDefaultVisual {
 
 	@Override
 	public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Token> params, Map<String, MemoryAPI> memoryMap) {
+
+		// Illustrated Entities compatibility; delegate to that mod's ShowDefaultVisual method
+		if (Global.getSettings().getModManager().isModEnabled("illustrated_entities")) {
+			BaseCommandPlugin ieVisual = (BaseCommandPlugin)NexUtils.instantiateClassByName("illustratedEntities.dialogue.rules.ShowDefaultVisualWithSelector");
+			ieVisual.execute(ruleId, dialog, params, memoryMap);
+			return true;
+		}
 		
 		SectorEntityToken target = dialog.getInteractionTarget();
 		String entityId = target.getId();
 		if (CUSTOM_MARKET_IMAGES.containsKey(entityId)) {
 			dialog.getVisualPanel().showImageVisual(new InteractionDialogImageVisual("illustrations", CUSTOM_MARKET_IMAGES.get(entityId), 640, 400));
-		} else if (target.getCustomInteractionDialogImageVisual() != null) {
-			dialog.getVisualPanel().showImageVisual(target.getCustomInteractionDialogImageVisual());
-		} else {
-			if (target instanceof PlanetAPI) {
-				dialog.getVisualPanel().showPlanetInfo((PlanetAPI) target);
-			} else if (target instanceof CampaignFleetAPI) {
-				CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
-				CampaignFleetAPI otherFleet = (CampaignFleetAPI) target;
-				//dialog.getVisualPanel().setVisualFade(0.25f, 0.25f);
-				//dialog.getVisualPanel().showFleetInfo((String)null, playerFleet, (String)null, otherFleet, null);
-				if (playerFleet != otherFleet)
-					showFleetInfo(dialog, playerFleet, otherFleet);
-			}
-			else {
-				MemoryAPI mem = memoryMap.get(MemKeys.MARKET);
-				for (String tag : CUSTOM_MARKET_TAGS) {
-					if (mem.contains(tag) && CUSTOM_MARKET_TAG_IMAGES.containsKey(tag))
-						dialog.getVisualPanel().showImageVisual(new InteractionDialogImageVisual("illustrations", CUSTOM_MARKET_TAG_IMAGES.get(tag), 640, 400));
-				}
-			}
+		}
+		else {
+			super.execute(ruleId, dialog, params, memoryMap);
 		}
 	
 		return true;
