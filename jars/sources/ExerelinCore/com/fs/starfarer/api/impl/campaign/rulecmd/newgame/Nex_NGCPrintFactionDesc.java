5b5f7bc4032e27487de1aeb212d9cd754ca328ec
L.J. Lim <kingraptorowns@gmail.com>
2025-02-22 23:08:39 +0800

    Standardised system for printing faction descriptions in new game dialog

---
+++ jars/sources/ExerelinCore/com/fs/starfarer/api/impl/campaign/rulecmd/newgame/Nex_NGCPrintFactionDesc.java
@@ -0,0 +1,38 @@
+package com.fs.starfarer.api.impl.campaign.rulecmd.newgame;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.rules.MemKeys;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+import com.fs.starfarer.api.loading.Description;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+import exerelin.utilities.StringHelper;
+
+import java.util.List;
+import java.util.Map;
+
+public class Nex_NGCPrintFactionDesc extends BaseCommandPlugin {
+
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        String factionId = memoryMap.get(MemKeys.LOCAL).getString("$playerFaction");
+        try {
+            Description desc = Global.getSettings().getDescription(factionId, Description.Type.FACTION);
+            FactionAPI faction = Global.getSector().getFaction(factionId);
+            TooltipMakerAPI tt = dialog.getTextPanel().beginTooltip();
+            TooltipMakerAPI sub = tt.beginImageWithText(faction.getCrest(), 64);
+            String str = desc.getText1FirstPara();
+            if (faction.isPlayerFaction()) {
+                str = StringHelper.getString("exerelin_ngc", "factionDesc_player");
+            }
+            sub.addPara(str, 0);
+            tt.addImageWithText(0);
+            dialog.getTextPanel().addTooltip();
+        } catch (Exception ex) {
+
+        }
+        return true;
+    }
+}
